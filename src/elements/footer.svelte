7d112259956aab0bc9c6323658af360c2884e711
Martin <martin.schreurs@netcologne.de>
2024-08-25 15:27:06 +0200

    moves copyright to bottom mid and add big logo in footer

--- src/elements/footer.svelte
+++ src/elements/footer.svelte
@@ -1,43 +1,57 @@
 <footer>
-	<nav>
-		<ul>
-			<li><a href="/login">Login</a></li>
-			<li><a href="/login">Register</a></li>
-			<li><a href="/impressum">Impressum</a></li>
-		</ul>
-	</nav>
+	<div class="nav-wrapper">
+		<nav>
+			<ul>
+				<li><a href="/login">Login</a></li>
+				<li><a href="/login">Register</a></li>
+				<li><a href="/impressum">Impressum</a></li>
+			</ul>
+		</nav>
+		
+		<nav>
+			<p class="YearHeadline">Alle Test-Conf:</p>
+			<ul>
+				<li><a href="/year/2023">2023</a></li>
+				<li><a href="/year/2024">2024</a></li>
+			</ul>
+		</nav>
+		
+		<p>
+			Bester Beschreibungstext <br />
+			aös,jkdhfga <br />
+			sgasdg asdgasdgasd <br />
+			gasdgökajsdhga <br />
+			sdgaölkjskdhg
+		</p>
 
-	<nav>
-        <p class="YearHeadline">Alle Test-Conf:</p>
-		<ul>
-			<li><a href="/year/2023">2023</a></li>
-			<li><a href="/year/2024">2024</a></li>
-		</ul>
-	</nav>
+		<img src={"/logo_big.png"} alt="The small Test-Conf logo. It shows the name on black clouds."/>
+	</div>
 
-	<p>
-		Bester Beschreibungstext <br />
-		aös,jkdhfga <br />
-		sgasdg asdgasdgasd <br />
-		gasdgökajsdhga <br />
-		sdgaölkjskdhg
-	</p>
-
-	<p>&copy; 2024</p>
+	<p class="copyright">&copy; Tech Stream Conference 2024</p>
 </footer>
 
 <style>
 	footer {
 		display: flex;
-		justify-content: space-around;
+		flex-direction: column;
 		background-color: var(--primary-color);
-		padding: 2rem 0;
+		padding: 3rem 0;
 		color: white;
 		font-family: 'Gnuolane Extra Light', sans-serif;
 		font-weight: 500;
 		font-style: normal;
 		letter-spacing: 2px;
 		font-size: 1.5rem;
+	}
+
+	.nav-wrapper {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+	}
+
+	img {
+		height: 10rem;
 	}
 
 	ul {
@@ -52,9 +66,10 @@
 		padding: 0.2rem 0;
 	}
 
-    .YearHeadline {
-        padding-left: 0.2rem;
-    }
+	.YearHeadline {
+		padding-left: 0.2rem;
+		padding-bottom: 1rem;
+	}
 
 	a:hover {
 		background-color: var(--third-color);
@@ -64,12 +79,19 @@
 	a {
 		text-decoration: none;
 		color: white;
-        padding: 0.3rem;
-        border-radius: 0.6rem;
+		padding: 0.3rem;
+		border-radius: 0.6rem;
 	}
 
 	p {
 		margin: 0;
 		padding: 0;
+	}
+
+	.copyright {
+		padding-top: 5rem;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
 	}
 </style>
