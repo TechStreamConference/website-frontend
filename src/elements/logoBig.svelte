b1a27e54cd3f53990896803f5a0a99a4cc24cb97
Martin <martin.schreurs@netcologne.de>
2024-08-26 23:18:27 +0200

    add wrapper for big logo

---
+++ src/elements/logoBig.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let classList = '';
+</script>
+
+<img
+	class={classList}
+	src={'/logo_big.png'}
+	alt="Das große Tech Stream Conference Logo. Es zeigt den Schriftzug auf schwarzen Wolken. Aus den Wolken kommen schwarze Schaltkreise an deren Enden gelber Punkte sind. Test ist in Großbuchstaben und weiß / grün. conf in Kleinbuchstaben darunter und weiß."
+/>
+
+<style>
+	.footer {
+		height: 7rem;
+	}
+</style>
