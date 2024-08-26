557eae98fa352c682a71b15c00a6439e38f7f0ab
Martin <martin.schreurs@netcologne.de>
2024-08-26 23:17:42 +0200

    add wrapper for small logo

---
+++ src/elements/logoSmall.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let classList = '';
+</script>
+
+<img
+	class={classList}
+	src={'/logo_small.png'}
+	alt="Das kleine Tech Stream Conference Logo. Es zeigt den Schriftzug auf schwarzen Wolken. Test ist in Großbuchstaben und weiß / grün. conf in Kleinbuchstaben darunter und weiß."
+/>
+
+<style>
+	.header {
+		height: 5rem;
+		margin-left: 1rem;
+	}
+</style>
