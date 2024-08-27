025d1d98e492085cb28d9f347dd1edc889c61371
Martin <martin.schreurs@netcologne.de>
2024-08-27 23:14:32 +0200

    typo

--- src/elements/logoBig.svelte
+++ src/elements/logoBig.svelte
@@ -5,7 +5,7 @@
 <img
 	class={classList}
 	src={'/logo_big.png'}
-	alt="Das große Tech Stream Conference Logo. Es zeigt den Schriftzug auf schwarzen Wolken. Aus den Wolken kommen schwarze Schaltkreise an deren Enden gelbe Punkte sind. Test ist in Großbuchstaben und weiß / grün. conf in Kleinbuchstaben darunter und weiß."
+	alt="Das große Tech Stream Conference Logo. Es zeigt den Schriftzug auf schwarzen Wolken. Aus den Wolken kommen schwarze Schaltkreise, an deren Enden gelbe Punkte sind. Test ist in Großbuchstaben und weiß / grün. conf in Kleinbuchstaben darunter und weiß."
 />
 
 <style>
