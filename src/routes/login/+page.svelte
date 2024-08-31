34b9f3e191d6e6401a52da918639c5f55aa5088e
Martin <martin.schreurs@netcologne.de>
2024-08-31 20:17:54 +0200

    add header to login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,37 @@
-<h1>Best login Page</h1>
+<script>
+	import Header from 'elements/header.svelte';
+	import Footer from 'elements/footer.svelte';
+	import MainPage from 'elements/mainPage.svelte';
+
+	let currentYear = 2025; // get current year from database
+
+	const headerMenu = [['Home', '/ ']];
+	const footerMenu = [
+		['Register', '/login'],
+		['Impressum', '/impressum']
+	];
+</script>
+
+<Header menu={headerMenu} />
+
+<div class="page">
+	<MainPage />
+
+	<div class="content">
+        <p>content here</p>
+    </div>
+
+	<Footer {currentYear} menu={footerMenu} />
+</div>
+
+<style>
+	.page {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	.content {
+		flex-grow: 1;
+	}
+</style>
