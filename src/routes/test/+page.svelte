bb89496a4bebb0ad1b1c1a889770b085fef6ac69
Martin <martin.schreurs@netcologne.de>
2024-08-31 22:02:51 +0200

    font style testing

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,12 @@
+<h1 class="headline">content here</h1>
+<h1 class="headline-small">content here</h1>
+<h2 class="sub-headline">content here</h2>
+<h2 class="sub-headline-small">content here</h2>
+<p class="text-line">content here</p>
+<p class="text-block">
+	content hereapsjkdgfhflaksjdghlaskjdhfglaskjdhflakjsdhflkajshdflkajsdhflkjashdfl
+	kjajshdflkjahsdflkjahssd
+	flkjashsdflkjahdsflkjahdsflkjahdsflkjhadsflkjhadflkjhadsflkjhadslfkjhadslkjfhaslkfha
+	lskjhflakjshflakjshdflkajhdsflkjashdflkjajhsdflkjahsdflkjhasdflkjhasdflkjhasdlfkjhasdlkf
+	jhadslkfjhasldkjfhalkjsdfhalkjdfhalkjdsfhalkjdshflkjadhflkjasdhflkjasdhlkajfh
+</p>
