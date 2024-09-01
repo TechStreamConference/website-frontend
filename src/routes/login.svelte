26315185ff665d3b643348a95e7e10942953e2bc
Martin <martin.schreurs@netcologne.de>
2024-09-01 13:18:47 +0200

    imput lines and styling

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -9,12 +9,20 @@
 	export let headerMenu = [['', '']];
 	export let footerMenu = [['', '']];
 
+	let username = '';
+
+	let usernameMessage = '';
+
 	function login() {
 		console.log('TODO: login');
 	}
 
 	function register() {
 		console.log('TODO: register');
+	}
+
+	function usernameChance() {
+		usernameMessage = username;
 	}
 </script>
 
@@ -27,14 +35,52 @@
 		{#if displayRegister}
 			<div class="content-wrapper">
 				<h1 class="headline-small center-text">Register</h1>
+				<div class=my-line-horizontal></div>
+				<div class="input-line-wrapper">
+					<label for="username" class="my-label">Username:</label>
+					<input
+						type="text"
+						name="username"
+						placeholder="username"
+						class="my-input-line"
+						on:input={usernameChance}
+						bind:value={username}
+					/>
+					<label for="email" class="my-label">E-Mail:</label>
+					<input type="text" name="email" placeholder="e-mail" class="my-input-line" />
+					<label for="password" class="my-label">Password:</label>
+					<input type="password" name="password" placeholder="passowrd" class="my-input-line" />
+					<label for="password" class="my-label">Password:</label>
+					<input
+						type="password-repeated"
+						name="password-repeated"
+						placeholder="passowrd"
+						class="my-input-line"
+					/>
+				</div>
 				<button class="my-button" on:click={register}>Register</button>
 			</div>
 		{:else}
 			<div class="content-wrapper">
 				<h1 class="headline-small center-text">Login</h1>
+				<div class=my-line-horizontal></div>
+				<div class="input-line-wrapper">
+					<label for="username" class="my-label">Username:</label>
+					<input type="text" name="username" placeholder="username" class="my-input-line" />
+					<label for="email" class="my-label">E-Mail:</label>
+					<input type="text" name="email" placeholder="e-mail" class="my-input-line" />
+					<label for="password" class="my-label">Password:</label>
+					<input type="password" name="password" placeholder="passowrd" class="my-input-line" />
+				</div>
 				<button class="my-button" on:click={login}>Login</button>
 			</div>
 		{/if}
+
+		<div class="message-wrapper">
+			{#if usernameMessage !== ''}
+				<p class="error-message">{usernameMessage}</p>
+			{/if}
+		</div>
 	</div>
 
 	<Footer {currentYear} menu={footerMenu} />
@@ -50,7 +96,7 @@
 	.content {
 		flex-grow: 1;
 		display: flex;
-		flex-direction: row;
+		flex-direction: column;
 		margin: 3rem;
 	}
 
@@ -65,13 +111,38 @@
 	}
 
 	.headline-small {
-		margin-top: 0;
+		margin: 0;
+	}
+
+	.my-line-horizontal {
+		width: 30rem;
+		margin-bottom:5rem;
+	}
+
+	.input-line-wrapper {
+		display: flex;
+		flex-direction: column;
+		margin: 0 auto;
+	}
+
+	.my-input-line {
+		margin-bottom: 1rem;
+		width: 30rem;
 	}
 
 	.my-button {
+		margin: 3rem auto 0 auto;
+	}
+
+	.message-wrapper {
+		display: flex;
+		flex-direction: column;
 		margin: 0 auto;
-		height: 3rem;
-		width: 7rem;
+	}
+
+	.error-message {
+		background-color: red;
+		color: var(--text-color);
 	}
 
 	@media (max-width: 1280px) {
