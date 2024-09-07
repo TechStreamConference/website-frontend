f96ed2b597552de1537ff56e0c54432548ae1288
Martin <martin.schreurs@netcologne.de>
2024-09-07 23:48:11 +0200

    import Page -> import Year

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-	import Page from './year/[year]/year.svelte';
+	import Year from './year/[year]/year.svelte';
 	let currentYear = 2025; // get current year from database
 </script>
 
-<Page displayedYear={currentYear} {currentYear} />
+<Year displayedYear={currentYear} {currentYear} />
