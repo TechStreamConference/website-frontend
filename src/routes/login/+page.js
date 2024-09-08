0b79dc8ca7ad401e0c5fb30e2478b91137781caf
Martin <martin.schreurs@netcologne.de>
2024-09-08 12:23:36 +0200

    redirect login

---
+++ src/routes/login/+page.js
@@ -0,0 +1,11 @@
+import { redirect } from '@sveltejs/kit';
+
+export async function load({ fetch }) {
+    const result = await fetch('api/account');
+
+    if (result.ok) {
+        throw redirect(302, '/');
+    }
+
+    return {};
+}
