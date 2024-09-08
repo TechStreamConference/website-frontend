fa33d1839c567c1f52fee1ce11dcd87635921267
Martin <martin.schreurs@netcologne.de>
2024-09-08 14:06:47 +0200

    dummy impressum

--- src/routes/impressum/+page.svelte
+++ src/routes/impressum/+page.svelte
@@ -1,1 +1,28 @@
-<h1>Bestes Impressum. Also wirklich.</h1>
+<script>
+	import Headline from 'elements/text/headline.svelte';
+	import TextLine from 'elements/text/textline.svelte';
+	import Spacer from 'elements/spacer/spacer.svelte';
+	import Button from 'elements/input/button.svelte';
+</script>
+
+<div class="wrapper">
+	<Headline>Impressum</Headline>
+	<Spacer --height="3rem" />
+	<TextLine --text-align="center">TODO: Hier kommt das Beste Impressum hin.</TextLine>
+	<Spacer --height="5rem" />
+	<Button
+		on:click={() => {
+			window.location.href = '/';
+		}}>Start</Button
+	>
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		margin: 0 auto;
+		min-height: 100vh;
+	}
+</style>
