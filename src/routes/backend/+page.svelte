835576761423611e3e223083d563724a7725fda2
Martin <martin.schreurs@netcologne.de>
2024-09-08 14:06:32 +0200

    dummy backend

--- src/routes/backend/+page.svelte
+++ src/routes/backend/+page.svelte
@@ -1,1 +1,28 @@
-<p>bestes Backend</p>
+<script>
+	import Headline from 'elements/text/headline.svelte';
+	import TextLine from 'elements/text/textline.svelte';
+	import Spacer from 'elements/spacer/spacer.svelte';
+	import Button from 'elements/input/button.svelte';
+</script>
+
+<div class="wrapper">
+	<Headline>Backend</Headline>
+	<Spacer --height="3rem" />
+	<TextLine --text-align="center">TODO: Hier kommt das Beste Backend hin.</TextLine>
+	<Spacer --height="5rem" />
+	<Button
+		on:click={() => {
+			window.location.href = '/';
+		}}>Start</Button
+	>
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		margin: 0 auto;
+		min-height: 100vh;
+	}
+</style>
