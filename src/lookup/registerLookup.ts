cdc79a30b2dda546da1a80d70e5c70316f2787f1
Martin <martin.schreurs@netcologne.de>
2024-09-18 10:03:55 +0200

    found a nother message

--- src/lookup/registerLookup.ts
+++ src/lookup/registerLookup.ts
@@ -4,17 +4,20 @@ type RegisterDict = {
 
 const lookup: RegisterDict = {
     // username
-    "The username field is required.": "Server: Das Namens Feld ist erforderlich.",
+    "The username field is required.": "Server: Das Feld 'Namen' ist erforderlich.",
     "The username field must be at least 3 characters in length.": "Server: Der Name muss mindestens aus 3 Zeichen bestehen.",
     "The username field cannot exceed 30 characters in length.": "Server: Der Name darf maximal aus 30 Zeichen bestehen.",
 
     // mail
-    "The email field is required.": "Server: Das E-Mail Feld ist erforderlich.",
+    "The email field is required.": "Server: Das Feld 'E-Mail' ist erforderlich.",
     "The email field must contain a valid email address.": "Server: Das E-Mail-Feld muss eine gültige E-Mail-Adresse enthalten",
+    
+    // username or mail
+    "Username or email already taken" : "Name oder E-Mail sind bereits vergeben",
 
     // password
-    "The password field is required.": "Server: Das Passwort Feld ist erforderlich.",
-    "PASSWORD_MISSING_FIELD": "Server: Das Passwort Feld ist erforderlich.",
+    "The password field is required.": "Server: Das Feld 'Passwort' ist erforderlich.",
+    "PASSWORD_MISSING_FIELD": "Server: Das Feld 'Passwort' ist erforderlich.",
     "PASSWORD_TOO_SHORT": "Server: Dein Passwort ist zu kurz. Mindestanzahl: 8 Zeichen",
     "PASSWORD_MISSING_LOWERCASE_LETTER": "Server: Deinem Passwort fehlt ein kleiner Buchstabe.",
     "PASSWORD_MISSING_UPPERCASE_LETTER": "Server: Deinem Passwort fehlt ein großer Buchstabe.",
