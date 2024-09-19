2e6d1f8aff42e0e81ef1ca575d197bf7013a8281
Martin <martin.schreurs@netcologne.de>
2024-09-19 22:25:34 +0200

    link style

--- src/toDelete/dummyIcon.svelte
+++ src/toDelete/dummyIcon.svelte
@@ -10,5 +10,21 @@
 		width: 2rem;
 		height: 2rem;
 		background-color: var(--primary-color);
+		transition: var(--transition-duration);
+		border-radius: var(--border-radius);
+	}
+
+	.img:hover {
+		background-color: var(--secondary-color);
+		box-shadow: 4px 4px var(--gray-color);
+		transform: translateY(-4px) translateX(-4px);
+		transition: var(--transition-duration);
+	}
+
+	.img:active {
+		background-color: var(--third-color);
+		box-shadow: 1px 1px var(--light-gray-color);
+		transform: translateY(0px) translateX(0px);
+		transition: var(--fast-transition-duration);
 	}
 </style>
