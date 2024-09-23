5c6672c39ecc7f68897e3a5a86846545463032f9
Martin <martin.schreurs@netcologne.de>
2024-09-23 21:18:28 +0200

    formatter

--- src/elements/person/personImage.svelte
+++ src/elements/person/personImage.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import { personImageUrl } from "helper/links";
-	import type { Person } from "types/provideTypes";
+	import { personImageUrl } from 'helper/links';
+	import type { Person } from 'types/provideTypes';
 
 	export let classes: string = '';
-    export let data:Person;
+	export let data: Person;
 </script>
 
-<img class={classes} src={personImageUrl(data.photo)} alt={"Profilbild von " + data.name} />
+<img class={classes} src={personImageUrl(data.photo)} alt={'Profilbild von ' + data.name} />
