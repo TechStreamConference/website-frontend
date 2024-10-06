61e16dc19d3d7c5c7698dd9267279de41fee4d75
Martin <martin.schreurs@netcologne.de>
2024-10-06 13:50:39 +0200

    adding an image

---
+++ src/elements/image/image.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let classes: string = 'size-default';
+	export let alt: string;
+	export let src: string;
+</script>
+
+<img class={classes} {src} {alt} />
+
+<style>
+	.size-default {
+		max-height: 100%;
+		max-width: 100%;
+		height: auto;
+		width: auto;
+	}
+</style>
