6c3b234e44667937ccc484a6917c24fcc3d072ba
Martin <martin.schreurs@netcologne.de>
2024-10-06 13:51:13 +0200

    adds an image links

---
+++ src/elements/image/imageLink.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { Image } from 'types/provideTypes';
+	import Img from './image.svelte';
+	export let image: Image;
+
+	export let classesLink: string = 'size-default';
+</script>
+
+<a
+	href={image.url}
+	target="_blank"
+	rel="noopener noreferrer"
+	class={classesLink}
+	title="Klicke hier um zu {image.name} zu navigieren."
+	on:click
+>
+	<Img src={image.image_url} alt={image.description} />
+</a>
+
+<style>
+	a {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+    .size-default {
+		max-height: 100%;
+		max-width: 100%;
+		height: auto;
+		width: auto;
+	}
+</style>
