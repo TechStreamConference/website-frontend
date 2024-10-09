9f407f03cff2c106e5c365e95d6bfc64b62f8ee4
Martin <martin.schreurs@netcologne.de>
2024-10-09 19:56:00 +0200

    move hover-move into an extra file.

--- src/elements/image/iconLink.svelte
+++ src/elements/image/iconLink.svelte
@@ -5,41 +5,13 @@
 	export let title: string;
 </script>
 
-<a href={linkType.url} target="_blank" rel="noopener noreferrer" class="icon" {title} on:click>
+<a href={linkType.url} target="_blank" rel="noopener noreferrer" class="hover-move icon" {title} on:click>
 	<Icon type={linkType.name} />
 </a>
 
 <style>
 	.icon {
 		background-color: var(--primary-color-light);
-		transition: var(--transition-duration);
 		border-radius: var(--border-radius);
-	}
-
-	.icon:hover {
-		background-color: var(--primary-color-mid);
-		box-shadow: 4px 4px var(--gray-color);
-		transform: translateY(-4px) translateX(-4px);
-		transition: var(--transition-duration);
-	}
-
-	.icon:active {
-		background-color: var(--primary-color-dark);
-		box-shadow: 1px 1px var(--gray-color-light);
-		transform: translateY(0px) translateX(0px);
-		transition: var(--transition-duration-fast);
-	}
-
-	@media (max-width: 1280px) {
-		.icon:hover {
-			background-color: var(--primary-color-light);
-			box-shadow: none;
-			transition: var(--transition-duration);
-		}
-
-		.icon:active {
-			box-shadow: 2px 2px var(--gray-color-light);
-			transition: var(--transition-duration-fast);
-		}
 	}
 </style>
