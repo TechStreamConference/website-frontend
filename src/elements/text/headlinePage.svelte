80946927a3b5a93ec965873316a23a6d01eb63af
Martin <martin.schreurs@netcologne.de>
2024-10-09 19:04:38 +0200

    global in text

--- src/elements/text/headlinePage.svelte
+++ src/elements/text/headlinePage.svelte
@@ -3,7 +3,7 @@
 	export let classes: string = '';
 </script>
 
-<div class="wrapper">
+<div class="wrapper text-headline-page">
 	<Headline classes="headline border {classes}"><slot /></Headline>
 </div>
 
@@ -13,14 +13,14 @@
 		width: fit-content;
 	}
 
-	:global(.headline) {
+	.text-headline-page :global(.headline) {
 		padding: 0 3rem;
 		margin-top: 9rem;
 		width: fit-content;
 	}
 
 	@media (max-width: 1280px) {
-		:global(.headline) {
+		.text-headline-page :global(.headline) {
 			padding: 0 2rem;
 			margin-top: 6rem;
 			width: fit-content;
@@ -28,7 +28,7 @@
 	}
 
 	@media (max-width: 600px) {
-		:global(.headline) {
+		.text-headline-page :global(.headline) {
 			padding: 0 1rem;
 			width: fit-content;
 		}
