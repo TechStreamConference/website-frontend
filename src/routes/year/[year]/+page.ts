62af8edbcdee830c91c56f88ca3e7898072346cc
Martin <martin.schreurs@netcologne.de>
2024-10-10 22:03:07 +0200

    call new error page

--- src/routes/year/[year]/+page.ts
+++ src/routes/year/[year]/+page.ts
@@ -1,12 +1,12 @@
 import type { LoadYearPromise } from "types/loadTypes";
-import { redirect } from "@sveltejs/kit";
+import { error } from "@sveltejs/kit";
 
 import { loadYearAsync } from "./year";
 
 export async function load({ fetch, params }: { fetch: typeof globalThis.fetch, params: { year: string } }): LoadYearPromise {
     const year: number = Number(params.year);
     if (Number.isNaN(year)) {
-        redirect(302, "/404");
+        throw error(404, `in der URL angegebenes Jahr "${params.year}" ist keine Zahl.`);
     }
     return await loadYearAsync(fetch, year);
 }
