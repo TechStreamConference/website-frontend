c0b79f41483d049da9c49b05475cf403914006f5
Martin <martin.schreurs@netcologne.de>
2024-10-10 21:51:34 +0200

    adds a real error page

---
+++ src/menu/404.ts
@@ -0,0 +1,12 @@
+import type { Menu } from "types/provideTypes";
+import * as Item from "menu/menuItems";
+
+export const header: Menu = [
+    Item.start,
+    Item.imprint,
+];
+
+export const footer: Menu = [
+    Item.start,
+    Item.imprint,
+];
---
+++ src/routes/+error.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import * as Menu from 'menu/404';
+
+	import Header from 'elements/navigation/header.svelte';
+	import Footer from 'elements/navigation/footer.svelte';
+
+	import Button from 'elements/input/button.svelte';
+	import Headline from 'elements/text/headline.svelte';
+	import SubHeadline from 'elements/text/subHeadline.svelte';
+
+	function onClick() {
+		goto('/');
+	}
+</script>
+
+<Header menu={Menu.header} />
+<div class="wrapper page-404">
+	<div class="content">
+		<Headline classes="border">{$page.status}</Headline>
+		{#if $page.error}
+			<SubHeadline classes="subheadline">Fehler: {$page.error.message}</SubHeadline>
+		{/if}
+
+		<Button classes="text button" ariaLabel="Klicke um zur Hauptseite zu navigieren" on:click={onClick}
+			>Hauptseite</Button
+		>
+	</div>
+	<Footer menu={Menu.footer} currentYear={2024} />
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	.content {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex-grow: 1;
+	}
+
+	.page-404 :global(.subheadline) {
+		margin-top: 2rem;
+	}
+
+	.page-404 :global(.button) {
+		margin-top: 5rem;
+	}
+</style>
