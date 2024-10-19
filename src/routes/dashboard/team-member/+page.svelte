56ab428afc36e377319324a6baa5796a110d90a4
Martin <martin.schreurs@netcologne.de>
2024-10-19 21:51:35 +0200

    add team member route

---
+++ src/routes/dashboard/team-member/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	import HeadlinePage from 'elements/text/headlinePage.svelte';
+	import TextLine from 'elements/text/textLine.svelte';
+	import Button from 'elements/input/button.svelte';
+</script>
+
+<div class="wrapper page-dashboard-team-member">
+	<HeadlinePage classes="headline">Team Member</HeadlinePage>
+	<TextLine classes="text" --text-align="center"
+		>TODO: Hier kommt das Beste Team Member hin.</TextLine
+	>
+	<Button
+		classes="text button"
+		ariaLabel="Kehre zur akruellen Hautseite zurück"
+		on:click={() => {
+			goto('/');
+		}}
+	>
+		Start
+	</Button>
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		margin: 0 auto;
+		min-height: 100vh;
+	}
+
+	.page-dashboard-team-member :global(.headline) {
+		margin-bottom: 3rem;
+	}
+
+	.page-dashboard-team-member :global(.text) {
+		margin-bottom: 5rem;
+	}
+
+	.page-dashboard-team-member :global(.button) {
+		margin: 0 auto;
+	}
+</style>
