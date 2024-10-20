2587e103a0fdf1be001b1be33a9307c6c22204b3
Martin <martin.schreurs@netcologne.de>
2024-10-20 17:18:02 +0200

    add section for backend

---
+++ src/elements/section/sectionBackend.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let id: string = '';
+	export let classes: string = '';
+</script>
+
+<section {id} class="section {classes}"><slot /></section>
+
+<style>
+	.section {
+		margin: 2rem auto;
+	}
+</style>
