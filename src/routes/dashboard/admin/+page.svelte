fb012eac6fb58c5a0e851c0bf789bc9ebc88c1e7
Martin <martin.schreurs@netcologne.de>
2024-10-20 17:19:10 +0200

    add tabs into admin page

--- src/routes/dashboard/admin/+page.svelte
+++ src/routes/dashboard/admin/+page.svelte
@@ -5,16 +5,56 @@
 	import * as Menu from 'menu/dashboard';
 	import Header from 'elements/navigation/header.svelte';
 	import Footer from 'elements/navigation/footer.svelte';
+	import Tabs from 'elements/navigation/tabs.svelte';
 
 	import HeadlinePage from 'elements/text/headlinePage.svelte';
 	import TextLine from 'elements/text/textLine.svelte';
+
+	enum Tab {
+		Global = 0,
+		Two = 1,
+		Three = 2
+	}
+	let current: Tab = 0;
+	function updateTabs(index: Tab): void {
+		current = index;
+	}
+	const tabsEntries: { entry: string; callback: Function }[] = [
+		{
+			entry: 'Global',
+			callback: () => {
+				updateTabs(Tab.Global);
+			}
+		},
+		{
+			entry: 'entry 2',
+			callback: () => {
+				updateTabs(Tab.Two);
+			}
+		},
+		{
+			entry: 'entry 3',
+			callback: () => {
+				updateTabs(Tab.Three);
+			}
+		}
+	];
 </script>
 
 <Header menu={Menu.headerIn(data.roles)} />
 <div class="wrapper page-dashboard-admin">
 	<div class="content">
 		<HeadlinePage classes="headline">Admin</HeadlinePage>
-		<TextLine classes="text" --text-align="center">TODO: Hier kommt das Beste Admin hin.</TextLine>
+		<Tabs entries={tabsEntries} />
+		{#if current === Tab.Global}
+			<TextLine>Tab 1</TextLine>
+		{/if}
+		{#if current === Tab.Two}
+			<TextLine>Tab 2</TextLine>
+		{/if}
+		{#if current === Tab.Three}
+			<TextLine>Tab 3</TextLine>
+		{/if}
 	</div>
 
 	<Footer menu={Menu.footerIn} globals={data.globals} />
