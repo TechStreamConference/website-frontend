c4d6e372d940b28f7fa3c40607c57dfe0d75ae3c
Martin <martin.schreurs@netcologne.de>
2024-10-23 18:45:19 +0200

    combine 2 function with same api call

--- src/helper/loggedIn.ts
+++ src/helper/loggedIn.ts
@@ -1,15 +1,25 @@
 import { redirect } from '@sveltejs/kit';
 import { apiUrl } from './links';
+import { checkAndParseInputDataAsync } from './parseJson';
+import { rolesScheme, type Roles } from 'types/provideTypes';
 
 export async function fetchLoginStatusAsync(fetch: Function): Promise<boolean> {
     const response: Response = await fetch(apiUrl('/api/account/roles'));
     return response.ok;
 }
 
-export async function redirectIfUnauthorizedAsync(fetch: Function) {
-    const status: boolean = await fetchLoginStatusAsync(fetch);
-    if (!status) {
+export async function redirectIfUnauthorizedOrReteturnRolesAsync(fetch: Function): Promise<Roles> {
+    const response: Response = await fetch(apiUrl('/api/account/roles'));
+    if (!response.ok) {
         throw redirect(302, '/login?showLoginMessage=true');
     }
-    return status;
+
+    const roles: Roles = await checkAndParseInputDataAsync<Roles>(
+        response,
+        rolesScheme,
+        `Serveranfrage für roles nicht erfolgreich. throw error(404)`,
+        `Unerwartete Daten für roles. throw error(404)`
+    );
+
+    return roles;
 }
