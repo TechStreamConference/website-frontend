fad5c88bef9f8fbc4755ca5171a2bb80506d70df
Martin <martin.schreurs@netcologne.de>
2024-10-24 20:24:27 +0200

    data resetter

---
+++ src/elements/resetters/dataResetter.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { afterNavigate } from '$app/navigation';
+	import { dashboardValue, setDashboardValue, unsetDashboardValue } from 'stores/dashboard';
+	import { onMount } from 'svelte';
+
+	export let data: any;
+
+	onMount(() => {
+		setDashboardValue(data);
+	});
+
+	afterNavigate(() => {
+		data = dashboardValue();
+		unsetDashboardValue();
+	});
+</script>
