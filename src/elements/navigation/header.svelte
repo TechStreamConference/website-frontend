b499c33e8c52029467b08a9e7847a3efb6d84f81
Martin <martin.schreurs@netcologne.de>
2024-10-27 18:21:07 +0100

    new fontsize in header

--- src/elements/navigation/header.svelte
+++ src/elements/navigation/header.svelte
@@ -19,19 +19,19 @@
 
 <header class="navigation-header">
 	<!-- Desktop -->
-	<div class="desktop-wrapper">
+	<div class="navigation-header-desktop-wrapper">
 		<a
 			href="/"
-			class="logo-wrapper"
+			class="navigation-header-logo-wrapper"
 			title="Tech Stream Conference Seite des aktuellen Jahres anschauen"
 		>
-			<LogoSmall classes={'logo'} />
+			<LogoSmall classes={'navigation-header-logo'} />
 		</a>
 		<nav>
 			<ul>
 				{#each menu as entry}
 					<li>
-						<Link classes="standard link" href={entry.url} title={entry.description}>{entry.name}</Link>
+						<Link classes="standard navigation-header-link" href={entry.url} title={entry.description}>{entry.name}</Link>
 					</li>
 				{/each}
 			</ul>
@@ -39,26 +39,26 @@
 	</div>
 
 	<!-- Mobile -->
-	<div class="mobile-wrapper">
-		<div class="hamburger-wrapper {isOpen ? 'open' : 'close'}">
+	<div class="navigation-header-mobile-wrapper">
+		<div class="navigation-header-hamburger-wrapper {isOpen ? 'open' : 'close'}">
 			<a
 				href="/"
-				class="logo-wrapper"
+				class="navigation-header-logo-wrapper"
 				title="Tech Stream Conference Seite des aktuellen Jahres anschauen"
 			>
-				<LogoSmall classes={'logo'} />
+				<LogoSmall classes={'navigation-header-logo'} />
 			</a>
 
-			<button class="hamburger" aria-label="Menü aufklappen" on:click={toggleOpen}>
+			<button class="navigation-header-hamburger" aria-label="Menü aufklappen" on:click={toggleOpen}>
 				<div class={isOpen ? 'rotated' : 'normal'}></div>
 				<div class={isOpen ? 'rotated' : 'normal'}></div>
 				<div class={isOpen ? 'rotated' : 'normal'}></div>
 			</button>
 		</div>
 		{#if isOpen}
-			<div class="mobile-menu" transition:fade={{ duration: 300 }}>
+			<div class="navigation-header-mobile-menu" transition:fade={{ duration: 300 }}>
 				{#each menu as entry}
-					<Link classes="standard link" href={entry.url} title={entry.description}
+					<Link classes="standard navigation-header-link" href={entry.url} title={entry.description}
 								on:click={collapse}>{entry.name}</Link>
 				{/each}
 			</div>
@@ -68,10 +68,6 @@
 
 <style>
     /* Desktop */
-    .mobile-wrapper {
-        display: none;
-    }
-
     header {
         position: sticky;
         top: 0;
@@ -79,7 +75,11 @@
         z-index: 10;
     }
 
-    .desktop-wrapper {
+    .navigation-header-mobile-wrapper {
+        display: none;
+    }
+
+    .navigation-header-desktop-wrapper {
         background-color: var(--primary-color-light);
         display: flex;
         align-items: center;
@@ -89,15 +89,15 @@
         border-bottom: solid 1px black;
     }
 
-    .navigation-header :global(.logo) {
-        height: 5rem;
+    :global(.navigation-header-logo) {
+        height: 8rem;
         margin-left: var(--full-margin);
     }
 
     ul {
         display: flex;
         margin: var(--full-margin);
-        gap: var(--full-gap);
+        gap: var(--2x-gap);
     }
 
     li {
@@ -106,30 +106,31 @@
         transition: color var(--transition-duration);
     }
 
-    .navigation-header :global(.link) {
-        padding: var(--full-padding);
+    :global(.navigation-header-link) {
         color: var(--white-color);
         text-transform: uppercase;
+        font-size: var(--1-5x-font-size) !important;
+        padding: var(--full-padding) !important;
     }
 
     /* Mobile */
     @media (max-width: 1280px) {
-        .desktop-wrapper {
+        .navigation-header-desktop-wrapper {
             display: none;
         }
 
-        .mobile-wrapper {
+        .navigation-header-mobile-wrapper {
             display: flex;
             flex-direction: column;
             justify-content: space-between;
         }
 
-        .navigation-header :global(.logo) {
-            height: 3rem;
+        :global(.navigation-header-logo) {
+            height: 6rem;
             margin-left: var(--full-margin);
         }
 
-        .hamburger-wrapper {
+        .navigation-header-hamburger-wrapper {
             background-color: var(--primary-color-light);
             display: flex;
             flex-direction: row;
@@ -139,17 +140,17 @@
             align-items: center;
         }
 
-        .hamburger-wrapper.open {
+        .navigation-header-hamburger-wrapper.open {
             border-bottom: none;
             transition: border var(--transition-duration);
         }
 
-        .logo-wrapper {
+        .navigation-header-logo-wrapper {
             display: inline-block;
             vertical-align: middle;
         }
 
-        .hamburger {
+        .navigation-header-hamburger {
             display: block;
             cursor: pointer;
             margin: var(--full-margin);
@@ -157,7 +158,7 @@
             border: none;
         }
 
-        .hamburger div {
+        .navigation-header-hamburger div {
             width: 25px;
             height: 3px;
             background-color: white;
@@ -165,41 +166,45 @@
             transition: var(--transition-duration);
         }
 
-        .hamburger div:nth-child(1).normal {
+        .navigation-header-hamburger div:nth-child(1).normal {
             transform: translateX(0) rotate(0) translateY(0);
         }
 
-        .hamburger div:nth-child(1).rotated {
-            transform: translateX(7px) rotate(45deg) translateY(0.72rem);
+        .navigation-header-hamburger div:nth-child(1).rotated {
+            transform: translateX(7px) rotate(45deg) translateY(11px);
         }
 
-        .hamburger div:nth-child(2).normal {
+        .navigation-header-hamburger div:nth-child(2).normal {
             opacity: 1;
         }
 
-        .hamburger div:nth-child(2).rotated {
+        .navigation-header-hamburger div:nth-child(2).rotated {
             opacity: 0;
         }
 
-        .hamburger div:nth-child(3).normal {
+        .navigation-header-hamburger div:nth-child(3).normal {
             transform: translateX(0) rotate(0) translateY(0);
         }
 
-        .hamburger div:nth-child(3).rotated {
-            transform: translateX(7px) rotate(-45deg) translateY(-0.72rem);
+        .navigation-header-hamburger div:nth-child(3).rotated {
+            transform: translateX(7px) rotate(-45deg) translateY(-11px);
         }
 
-        .mobile-menu {
+        .navigation-header-mobile-menu {
             background-color: var(--primary-color-light);
             flex-direction: column;
             display: flex;
             border-bottom: solid 1px black;
+            position: fixed;
+            left: 0;
+            right: 0;
+            top: 6rem;
         }
 
-        .mobile-menu :global(.link) {
+        :global(.navigation-header-link) {
             text-transform: uppercase;
             color: var(--white-color);
-						align-self: end;
+						align-self: end !important;
         }
     }
 </style>
