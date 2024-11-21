97939e8272a9b514262ff949988d16554d44111a
Martin <martin.schreurs@netcologne.de>
2024-11-21 19:04:18 +0100

    remove lambada

--- src/types/saveMessageType.ts
+++ src/types/saveMessageType.ts
@@ -9,15 +9,11 @@ export function isSaveType(type: SaveMessageType): boolean {
 }
 
 export function combineSaveType(lhs: SaveMessageType, rhs: SaveMessageType): SaveMessageType {
-    const check: Function = (lhs: SaveMessageType, rhs: SaveMessageType, reference: SaveMessageType): boolean => {
-        return lhs === reference || rhs === reference;
-    }
-
-    if (check(lhs, rhs, SaveMessageType.Error)) {
+    if (lhs === SaveMessageType.Error || rhs === SaveMessageType.Error) {
         return SaveMessageType.Error;
     }
 
-    if (check(lhs, rhs, SaveMessageType.None)) {
+    if (lhs === SaveMessageType.None || rhs === SaveMessageType.None) {
         return SaveMessageType.None;
     }
 
