07f9908047a76e481219578a6185a9af33f4648a
Martin <martin.schreurs@netcologne.de>
2024-11-26 23:48:57 +0100

    add a optional id in subheadline for srolling to anchor

--- src/elements/text/subHeadline.svelte
+++ src/elements/text/subHeadline.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	export let classes: string = '';
 	export let size: string = 'sub-headline-big';
+	export let id: string = '';
 </script>
 
-<h3 class="sub-headline-font {classes} {size}"><slot /></h3>
+<h3 id={id || undefined} class="sub-headline-font {classes} {size}"><slot /></h3>
 
 <style>
 	.sub-headline-big {
@@ -19,15 +20,15 @@
 		color: var(--white-color);
 	}
 
-	@media (max-width: 600px){
-			.sub-headline-big {
-					font-size: var(--full-font-size);
-					line-height: var(--full-line-height);
-			}
+	@media (max-width: 600px) {
+		.sub-headline-big {
+			font-size: var(--full-font-size);
+			line-height: var(--full-line-height);
+		}
 
-			.sub-headline-small {
-					font-size: var(--half-font-size);
-					line-height: var(--half-line-height);
-      }
+		.sub-headline-small {
+			font-size: var(--half-font-size);
+			line-height: var(--half-line-height);
+		}
 	}
 </style>
