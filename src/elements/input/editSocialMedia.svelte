798a10d6276da3861b4265a14077b82f83337a12
Martin <martin.schreurs@netcologne.de>
2024-12-02 23:32:05 +0100

    social media edit links

---
+++ src/elements/input/editSocialMedia.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import type { SocialMediaLinks } from 'types/provideTypes';
+	import DropDown from './dropDown.svelte';
+	import { socialMediaIconNames } from 'lookup/iconLookup';
+	import Input from './input.svelte';
+	import StyledLink from './styledLink.svelte';
+	import Button from './button.svelte';
+
+	export let links: SocialMediaLinks;
+	export let deleteCallback: (id: number) => void;
+</script>
+
+<div class="edit-social-media-links-wrapper">
+	{#each links as link, index}
+		<StyledLink
+			href={link.url}
+			icon={link.name}
+			title="Klicke um zur url von {link.name} zu navigieren"
+			classes="edit-social-media-links-link"
+		/>
+		<DropDown
+			id="edit-social-media-links-icon-{index}"
+			labelText="Link-Art:"
+			data={socialMediaIconNames}
+			bind:selected={link.name}
+			on:input
+			on:submit
+		/>
+		<Input
+			id="edit-social-media-links-link-{index}"
+			labelText="Link-URL:"
+			placeholderText="Link-URL"
+			bind:value={link.url}
+			ariaLabel="Trage die URL für {link.name} ein"
+			on:input
+		/>
+		<Button
+			buttonSize="small-button"
+			ariaLabel="Klicke hier um diesen Eintrag zu löschen."
+			classes="edit-social-media-links-link"
+			on:click={() => {
+				deleteCallback(index);
+			}}>Löschen</Button
+		>
+	{/each}
+</div>
+
+<style>
+	.edit-social-media-links-wrapper {
+		display: grid;
+		grid-template-columns: 5rem 1fr 1fr 11rem;
+		gap: var(--2x-gap);
+	}
+
+	:global(.edit-social-media-links-link) {
+		margin: auto auto 0 auto;
+	}
+</style>
