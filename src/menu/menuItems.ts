eed8c2e5dee430077849fdc6ff8b03008e83e863
Martin <martin.schreurs@netcologne.de>
2024-12-03 21:45:45 +0100

    typo

--- src/menu/menuItems.ts
+++ src/menu/menuItems.ts
@@ -4,86 +4,86 @@ export const mainPage: MenuItem =
 {
     name: 'Hauptseite',
     url: '/',
-    description: 'Klicke, um zur aktuellen Hauptseite zurückzukehren'
+    description: 'Klicke hier, um zur aktuellen Hauptseite zurückzukehren'
 }
 
 export const intern: MenuItem = {
     name: 'Intern',
     url: '/dashboard',
-    description: "Klicke, um zum internen Bereich zu navigieren"
+    description: "Klicke hier, um zum internen Bereich zu navigieren"
 }
 
 export const logout: MenuItem = {
     name: 'Abmelden',
     url: '/logout',
-    description: 'Klicke zum Abmelden'
+    description: 'Klicke hier zum Abmelden'
 }
 
 export const login: MenuItem = {
     name: 'Anmelden',
     url: '/login',
-    description: 'Klicke zum Anmelden'
+    description: 'Klicke hier zum Anmelden'
 }
 
 export const register: MenuItem = {
     name: 'Registrieren',
     url: '/register',
-    description: 'Klicke zum Registrieren'
+    description: 'Klicke hier zum Registrieren'
 }
 
 export const imprint: MenuItem = {
     name: 'Impressum',
     url: '/imprint',
-    description: 'Klicke, um zum Impressum zu gelangen'
+    description: 'Klicke hier, um zum Impressum zu gelangen'
 }
 
 export const adminGlobals: MenuItem = {
     name: 'Globals',
     url: '/dashboard/admin/globals',
-    description: 'Klicke hier, um zu den globalen Einstellungen im Admin-Dashboard zu gelangen.'
+    description: 'Klicke hier, um zu den globalen Einstellungen im Admin-Dashboard zu gelangen'
 };
 export const adminEvents: MenuItem = {
     name: 'Events',
     url: '/dashboard/admin/events',
-    description: 'Klicke hier, um zu den Event Einstellungen zu gelangen.'
+    description: 'Klicke hier, um zu den Event Einstellungen zu gelangen'
 };
 export const adminApproval: MenuItem = {
     name: 'Freigabe',
     url: '/dashboard/admin/approval',
-    description: 'Klicke hier, um ausstehende Freigaben zu bearbeiten.'
+    description: 'Klicke hier, um ausstehende Freigaben zu bearbeiten'
 };
 
 export const speakerEvents: MenuItem = {
     name: 'Events',
     url: '/dashboard/speaker/events',
-    description: 'Klicke hier um zu den Event spezifischen Einstellungen zu gelangen.'
+    description: 'Klicke hier, um zu den Event spezifischen Einstellungen zu gelangen'
 };
 export const speakerSocialMedia: MenuItem = {
     name: 'Social Media',
     url: '/dashboard/speaker/socials',
-    description: 'Klicke hier um zu den Social Media Einstellungen zu gelangen.'
+    description: 'Klicke hier, um zu den Social Media Einstellungen zu gelangen'
 };
 
 export const teamMemberEvents: MenuItem = {
     name: 'Events',
     url: '/dashboard/team-member/events',
-    description: 'Klicke hier um zu den Event spezifischen Einstellungen zu gelangen.'
+    description: 'Klicke hier, um zu den Event spezifischen Einstellungen zu gelangen'
 };
 export const teamMemberSocialMedia: MenuItem = {
     name: 'Social Media',
     url: '/dashboard/team-member/social-media',
-    description: 'Klicke hier um zu den Social Media Einstellungen zu gelangen.'
+    description: 'Klicke hier, um zu den Social Media Einstellungen zu gelangen'
 };
 
 export const userProfile: MenuItem = {
     name: 'Profil',
     url: '/dashboard/user/profile',
-    description: 'Klicke hier um das Profil zu bearbeiten.'
+    description: 'Klicke hier, um das Profil zu bearbeiten'
 }
 export const userCallForPapers: MenuItem = {
     name: 'Bewerbung',
     url: '/dashboard/user/call-for-papers',
-    description: 'Klicke hier um dich als Speaker für das aktuelle Event zu bewerben.'
+    description: 'Klicke hier, um dich als Speaker für das aktuelle Event zu bewerben'
 };
 
 export const admin: MenuItem = {
