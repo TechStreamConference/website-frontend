f85a9a012328eeb1a131eb71aa4bb4c6ff553111
Martin <martin.schreurs@netcologne.de>
2024-12-04 21:47:56 +0100

    add messages in speaker dashboards

--- src/routes/dashboard/speaker/events/+page.svelte
+++ src/routes/dashboard/speaker/events/+page.svelte
@@ -2,10 +2,16 @@
 	import * as Menu from 'menu/dashboard';
 	import * as MenuItem from 'menu/menuItems';
 
-	import List from 'elements/list/list.svelte';
-	import ListElement from 'elements/list/listElement.svelte';
-	import TextLine from 'elements/text/textLine.svelte';
+	import type { LoadDashboard, LoadSpeakerTeamMemberEvent } from 'types/dashboardLoadTypes';
+
 	import Tabs from 'elements/navigation/tabs.svelte';
+	import SectionDashboard from 'elements/section/sectionDashboard.svelte';
+	import DropDown from 'elements/input/dropDown.svelte';
+	import Message from 'elements/text/message.svelte';
+
+	export let data: LoadDashboard & LoadSpeakerTeamMemberEvent;
+	let selected: string = data.current.title;
+	let displayed: string = data.current.title;
 </script>
 
 <Tabs
@@ -13,15 +19,23 @@
 	entryName={MenuItem.speakerEvents.name}
 	classes="navigation-tabs-dashboard-subpage"
 />
-<TextLine>Drop Down with available Events</TextLine>
-<TextLine>Event Data from Speaker</TextLine>
-<br />
-<TextLine>Needed routes:</TextLine>
-<List classes="list-padding-left">
-	<ListElement classes="list-element-dot">
-		GET /api/dashboard/speaker/event/[:num] (all event specific data)
-	</ListElement>
-	<ListElement classes="list-element-dot">
-		PUT /api/dashboard/speaker/event/[:num] (all event specific data)
-	</ListElement>
-</List>
+
+<SectionDashboard>
+	<DropDown
+		id="dashboard-speaker-event-drop-down"
+		labelText="Event"
+		data={data.allEvents.map((x) => x.title)}
+		bind:selected
+	/>
+	{#if data.event.is_approved}
+		<Message message="Dieser Datensatz ist freigegeben" color="success" />
+	{:else}
+		<Message message="Dieser Datensatz muss noch freigegeben werden" />
+	{/if}
+	{#if data.event.requested_changes}
+		<Message
+			message={`Änderungswünsche:\n ${data.event.requested_changes}`}
+			classes="message-pre-wrap"
+		/>
+	{/if}
+</SectionDashboard>
