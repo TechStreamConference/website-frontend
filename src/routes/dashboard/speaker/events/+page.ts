2323f950c869e1eeec8ed6edc7ae7594014c5fd1
Martin <martin.schreurs@netcologne.de>
2024-12-05 20:50:21 +0100

    move specific year into seperate file

--- src/routes/dashboard/speaker/events/+page.ts
+++ src/routes/dashboard/speaker/events/+page.ts
@@ -1,10 +1,11 @@
 import type { LoadSpeakerTeamMemberEvent } from "types/dashboardLoadTypes";
-import type { DashboardAllEventID, DashboardAllSpeakerTeamMember } from "types/dashboardProvideTypes";
+import type { DashboardAllEventID, DashboardSpeakerTeamMember } from "types/dashboardProvideTypes";
 
-import { dashboardAllEventIDScheme, dashboardAllSpeakerTeamMemberScheme } from "types/dashboardProvideTypes";
+import { dashboardAllEventIDScheme } from "types/dashboardProvideTypes";
 import { error } from "@sveltejs/kit";
 import { apiUrl } from "helper/links";
 import { checkAndParseInputDataAsync } from "helper/parseJson";
+import { loadSpecificEvent } from "./loads";
 
 export async function load({ fetch }: { fetch: typeof globalThis.fetch }): Promise<LoadSpeakerTeamMemberEvent> {
     const allEventResponse: Promise<Response> = fetch(apiUrl('/api/dashboard/speaker/all-events'));
@@ -21,21 +22,11 @@ export async function load({ fetch }: { fetch: typeof globalThis.fetch }): Promi
     }
     const current = allEvents[allEvents.length - 1];
 
-    const eventResponse: Promise<Response> = fetch(apiUrl(`/api/dashboard/speaker/event/${current.event_id}`));
-    const event = await checkAndParseInputDataAsync<DashboardAllSpeakerTeamMember>(
-        await eventResponse,
-        dashboardAllSpeakerTeamMemberScheme,
-        `Serveranfrage für den Speaker-Eintrag für Event ${current.event_id} nicht erfolgreich. throw error(406)`,
-        `Unerwartete Daten für den Speaker-Eintrag für das Event ${current.event_id}. throw error(406)`
-    )
-    if (event.length != 1) {
-        console.error(`Empfangene Speaker Datensätze: ${event.length} | erwartete Speaker Datensätze: 1`)
-        throw error(406);
-    }
+    const event: DashboardSpeakerTeamMember = await loadSpecificEvent(fetch, current.event_id);
 
     return {
         allEvents,
         current,
-        event: event[0],
+        event,
     };
 }
