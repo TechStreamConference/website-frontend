14f242a8e9fe370a501cb1bd8856ef88a679935e
Martin <martin.schreurs@netcologne.de>
2024-12-06 23:29:24 +0100

    add local class for dashboard width

--- src/elements/section/sectionDashboard.svelte
+++ src/elements/section/sectionDashboard.svelte
@@ -2,7 +2,7 @@
 	export let classes: string = '';
 </script>
 
-<section class="{classes}"><slot /></section>
+<section class={classes}><slot /></section>
 
 <style>
 	section {
@@ -10,5 +10,12 @@
 		padding: 0 var(--4x-padding);
 		width: 100%;
 		align-self: center;
+	}
+
+	.standard-dashboard-section {
+		max-width: 100rem;
+		display: flex;
+		flex-direction: column;
+		gap: var(--full-gap);
 	}
 </style>
