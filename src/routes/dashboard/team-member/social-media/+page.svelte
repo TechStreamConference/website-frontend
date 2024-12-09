16d31e8a11a2a7a3cd66405008feda4c7b50aca9
Martin <martin.schreurs@netcologne.de>
2024-12-09 22:27:35 +0100

    use new files in team member page

--- src/routes/dashboard/team-member/social-media/+page.svelte
+++ src/routes/dashboard/team-member/social-media/+page.svelte
@@ -1,22 +1,8 @@
 <script lang="ts">
-	import * as Menu from 'menu/dashboard';
-	import * as MenuItem from 'menu/menuItems';
+	import SpeakerTeamMemberSocialMediaLinks from 'pages/speakerTeamMemberSocialMediaLinks.svelte';
+	import type { LoadDashboard, LoadUserSocials } from 'types/dashboardLoadTypes';
 
-	import List from 'elements/list/list.svelte';
-	import ListElement from 'elements/list/listElement.svelte';
-	import TextLine from 'elements/text/textLine.svelte';
-	import Tabs from 'elements/navigation/tabs.svelte';
+	export let data: LoadDashboard & LoadUserSocials;
 </script>
 
-<Tabs
-	entries={Menu.teamMember}
-	entryName={MenuItem.teamMemberSocialMedia.name}
-	classes="navigation-tabs-dashboard-subpage"
-/>
-<TextLine>Social Media Data from Team Member</TextLine>
-<br />
-<TextLine>Needed routes:</TextLine>
-<List classes="list-padding-left">
-	<ListElement classes="list-element-dot">GET /api/dashboard/team-member/socials</ListElement>
-	<ListElement classes="list-element-dot">PUT /api/dashboard/team-member/socials</ListElement>
-</List>
+<SpeakerTeamMemberSocialMediaLinks {data} />
