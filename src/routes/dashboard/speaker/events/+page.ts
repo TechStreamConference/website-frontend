cf304345a7cda0bf46a4e8a5e13c3fd296552ad4
Martin <martin.schreurs@netcologne.de>
2024-12-09 22:14:26 +0100

    use seperate laod function for speaker event

--- src/routes/dashboard/speaker/events/+page.ts
+++ src/routes/dashboard/speaker/events/+page.ts
@@ -1,32 +1,7 @@
 import type { LoadSpeakerTeamMemberEvent } from "types/dashboardLoadTypes";
-import type { DashboardAllEventID, DashboardSpeakerTeamMember } from "types/dashboardProvideTypes";
 
-import { dashboardAllEventIDScheme } from "types/dashboardProvideTypes";
-import { error } from "@sveltejs/kit";
-import { apiUrl } from "helper/links";
-import { checkAndParseInputDataAsync } from "helper/parseJson";
-import { loadSpecificEvent } from "./loads";
+import { loadDataAsync } from "pageHelper/loadSpeakerTeamMember";
 
 export async function load({ fetch }: { fetch: typeof globalThis.fetch }): Promise<LoadSpeakerTeamMemberEvent> {
-    const allEventResponse: Promise<Response> = fetch(apiUrl('/api/dashboard/speaker/all-events'));
-    const allEvents = await checkAndParseInputDataAsync<DashboardAllEventID>(
-        await allEventResponse,
-        dashboardAllEventIDScheme,
-        `Serveranfrage für alle Event IDs im Speaker nicht erfolgreich. throw error(406)`,
-        `Unerwartete Daten für Alle Event Ids der Speaker. throw error(406)`
-    )
-
-    if (allEvents.length == 0) {
-        console.error("Keine Events nach dem Laden von allen Events vorhanden.")
-        throw error(406);
-    }
-    const current = allEvents[allEvents.length - 1];
-
-    const event: DashboardSpeakerTeamMember = await loadSpecificEvent(fetch, current.event_id);
-
-    return {
-        allEvents,
-        current,
-        event,
-    };
+    return await loadDataAsync(fetch, "speaker");
 }
