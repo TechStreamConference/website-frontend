24ba91358907f5f8822b5e2c94f142be26a8ab2b
Martin <martin.schreurs@netcologne.de>
2024-12-19 23:43:46 +0100

    add a tooltip

---
+++ src/elements/text/tooltip.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	import Paragraph from './paragraph.svelte';
+
+	export let classes: string = '';
+	export let tooltipClasses: string = '';
+	export let tooltip: string;
+
+	let hovered: boolean = false;
+
+	let x: number = 0;
+	let y: number = 0;
+
+	function enter(event: MouseEvent) {
+		hovered = true;
+		move(event);
+	}
+	function leave() {
+		hovered = false;
+	}
+	function move(event: MouseEvent) {
+		x = event.pageX;
+		y = event.pageY;
+	}
+</script>
+
+<button
+	type="button"
+	class="tooltip-button-hover {classes}"
+	aria-label="Klicke oder Hover hier, um weite Infos zu erhalten."
+	on:mouseenter={enter}
+	on:mouseleave={leave}
+	on:mousemove={move}
+>
+	<slot />
+</button>
+
+{#if hovered}
+	<div style="top: {y}px; left: {x}px;" class="tooltip {tooltipClasses}">
+		<Paragraph classes="paragraph-pre-wrap paragraph-white" --text-align="center"
+			>{tooltip}</Paragraph
+		>
+	</div>
+{/if}
+
+<style>
+	.tooltip-button-hover {
+		height: auto;
+		width: auto;
+		background: none;
+		border: none;
+		border: 0;
+		cursor: pointer;
+	}
+
+	.tooltip-button-hover:hover,
+	.tooltip-button-hover:focus,
+	.tooltip-button-hover:active {
+		outline: none;
+		box-shadow: none;
+	}
+
+	.tooltip {
+		box-shadow: 2px 2px var(--gray-color-light);
+		background: var(--primary-color-dark);
+		border-radius: var(--border-radius);
+		padding: var(--full-padding);
+		position: absolute;
+		transform: translateX(-50%) translateY(3rem); /* Center the tooltip horizontally */
+	}
+</style>
