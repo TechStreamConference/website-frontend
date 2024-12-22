3d1aa1a14bc0a8bc24f53b855bd720a8b07482e1
Martin <martin.schreurs@netcologne.de>
2024-12-22 15:33:25 +0100

    style verify page

--- src/routes/verify-email-address/+page.svelte
+++ src/routes/verify-email-address/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+	import * as Menu from 'menu/verify';
+	import * as MenuItem from 'menu/menuItems';
+
+	import StyledLink from 'elements/input/styledLink.svelte';
 	import Footer from 'elements/navigation/footer.svelte';
 	import Header from 'elements/navigation/header.svelte';
+	import HeadlinePage from 'elements/text/headlinePage.svelte';
 	import TextLine from 'elements/text/textLine.svelte';
-	import * as Menu from 'menu/verify.js';
 
 	export let data;
 </script>
@@ -10,12 +14,42 @@
 <div class="verify-email-page-wrapper">
 	<Header menu={Menu.header} />
 	<div class="verify-email-content-wrapper">
+		<HeadlinePage>E-Mail Verifizierung</HeadlinePage>
 		{#if data.success === undefined}
-			<TextLine>E-Mail-Adresse wird verifiziert...</TextLine>
+			<div class="verify-email-text-wrapper">
+				<TextLine>👻 Wir versuchen gerade deine E-Mail zu verifizieren. 👻</TextLine>
+				<TextLine>Offenbar dauert das alles ein bisschen länger als wir erwartet haben.</TextLine>
+				<TextLine>Bitte hab etwas Geduld</TextLine>
+			</div>
+			<TextLine>Falls sich hier länger nix tun sollte melde dich gerne bei uns.</TextLine>
+			<StyledLink
+				text={MenuItem.imprint.name}
+				href={MenuItem.imprint.url}
+				title={MenuItem.imprint.description}
+				newTab={false}
+			/>
 		{:else if data.success}
-			<TextLine>E-Mail-Adresse wurde verifiziert.</TextLine>
+			<div class="verify-email-text-wrapper">
+				<TextLine>Deine E-Mail wurde erfolgreich verifiziert.</TextLine>
+				<TextLine>Du kannst dich ab jetzt anmelden.</TextLine>
+			</div>
+			<StyledLink
+				text={MenuItem.login.name}
+				href={MenuItem.login.url}
+				title={MenuItem.login.description}
+				newTab={false}
+			></StyledLink>
 		{:else}
-			<TextLine>Token ist ungültig.</TextLine>
+			<div class="verify-email-text-wrapper">
+				<TextLine>🫨 Verifizierung leider nicht erfolgreich. 🫨</TextLine>
+				<TextLine>Registriere dich bitte erneut, um eine neue Mail zu erhalten.</TextLine>
+			</div>
+			<StyledLink
+				text={MenuItem.register.name}
+				href={MenuItem.register.url}
+				title={MenuItem.register.description}
+				newTab={false}
+			/>
 		{/if}
 	</div>
 	<Footer globals={data.globals} menu={Menu.footer} />
@@ -30,5 +64,17 @@
 
 	.verify-email-content-wrapper {
 		flex-grow: 1;
+		max-width: 150rem;
+		align-self: center;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: var(--8x-gap);
+	}
+
+	.verify-email-text-wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 	}
 </style>
