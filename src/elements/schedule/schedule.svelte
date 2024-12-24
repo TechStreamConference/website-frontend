b511e7e1e4ab1a6da2db6243def54f974a1499f5
Martin <martin.schreurs@netcologne.de>
2024-12-24 13:34:48 +0100

    format person

--- src/elements/schedule/schedule.svelte
+++ src/elements/schedule/schedule.svelte
@@ -1,67 +1,69 @@
 <script lang="ts">
-	import type { Person, Talk } from 'types/provideTypes';
+    import type {Person, Talk} from 'types/provideTypes';
 
-	import ScheduleEntry from './scheduleEntry.svelte';
-	import SubHeadline from 'elements/text/subHeadline.svelte';
+    import ScheduleEntry from './scheduleEntry.svelte';
+    import SubHeadline from 'elements/text/subHeadline.svelte';
 
-	import { error } from '@sveltejs/kit';
-	import { formatDate } from 'helper/dates';
+    import {error} from '@sveltejs/kit';
+    import {formatDate} from 'helper/dates';
 
-	export let speakers: Person[];
-	export let schedule: Talk[];
-	export let personPopupCallback: (event: Event, person: Person) => void;
+    export let speakers: Person[];
+    export let schedule: Talk[];
+    export let personPopupCallback: (event: Event, person: Person) => void;
 
-	function getSpeakerById(id: number): Person {
-		for (const speaker of speakers) {
-			if (speaker.id === id) {
-				return speaker;
-			}
-		}
+    function getSpeakerById(id: number): Person {
+        for (const speaker of speakers) {
+            if (speaker.id === id) {
+                return speaker;
+            }
+        }
 
-		console.error(`error while looking up speaker with ID ${id}.`);
-		throw error(500);
-	}
+        console.error(`error while looking up speaker with ID ${id}.`);
+        throw error(500);
+    }
 </script>
 
 {#if schedule[0]}
-	<div class="schedule-element">
-		{#if schedule[0].is_special}
-			<!-- only using first element here because all elements should be the same by now -->
-			<SubHeadline>Bonusinhalte auf Youtube:</SubHeadline>
-		{/if}
-		<SubHeadline classes="schedule-element-sub-headline-schedule">
-			<!-- only using first element here because all elements should be the same by now -->
-			{formatDate(schedule[0].starts_at, '%d, %DD.%MM.%YYYY')}
-		</SubHeadline>
-		<div class="schedule-element-day">
-			{#each schedule as talk}
-				<ScheduleEntry speaker={getSpeakerById(talk.speaker_id)} {talk} {personPopupCallback} />
-			{/each}
-		</div>
-	</div>
+    <div class="schedule-element">
+        {#if schedule[0].is_special}
+            <!-- only using first element here because all elements should be the same by now -->
+            <SubHeadline>Bonusinhalte auf Youtube:</SubHeadline>
+        {/if}
+        <SubHeadline classes="schedule-element-sub-headline-schedule">
+            <!-- only using first element here because all elements should be the same by now -->
+            {formatDate(schedule[0].starts_at, '%d, %DD.%MM.%YYYY')}
+        </SubHeadline>
+        <div class="schedule-element-day">
+            {#each schedule as talk}
+                <ScheduleEntry speaker={getSpeakerById(talk.speaker_id)}
+                               {talk}
+                               {personPopupCallback}/>
+            {/each}
+        </div>
+    </div>
 {/if}
 
 <style>
-	.schedule-element {
-		margin-bottom: var(--2x-margin);
-	}
+    .schedule-element {
+        margin-bottom: var(--2x-margin);
+    }
 
-	:global(.schedule-element-sub-headline-schedule) {
-		border-bottom: 1px solid var(--text-color);
-		margin-bottom: var(--2x-margin);
-		padding-bottom: var(--quad-padding);
-	}
+    :global(.schedule-element-sub-headline-schedule) {
+        border-bottom:  1px solid var(--text-color);
+        margin-bottom:  var(--2x-margin);
+        padding-bottom: var(--quad-padding);
+    }
 
-	.schedule-element-day {
-		display: grid;
-		grid-template-columns: 16rem 1fr;
-		row-gap: var(--2x-gap);
-		margin: 0 var(--2x-margin) var(--4x-margin);
-	}
+    .schedule-element-day {
+        display:               grid;
+        grid-template-columns: 16rem 1fr;
+        row-gap:               var(--2x-gap);
+        margin:                0 var(--2x-margin) var(--4x-margin);
+    }
 
-	@media (max-width: 600px) {
-		.schedule-element-day {
-			grid-template-columns: 8rem 1fr;
-		}
-	}
+    @media (max-width: 600px) {
+        .schedule-element-day {
+            grid-template-columns: 8rem 1fr;
+        }
+    }
 </style>
--- src/elements/schedule/scheduleTag.svelte
+++ src/elements/schedule/scheduleTag.svelte
@@ -1,34 +1,34 @@
 <script lang="ts">
-	import type { TalkTag } from 'types/provideTypes';
+    import type {TalkTag} from 'types/provideTypes';
 
-	import TextLine from 'elements/text/textLine.svelte';
+    import TextLine from 'elements/text/textLine.svelte';
 
-	import { onMount } from 'svelte';
-	import { tagColorLookup, tagTextColorLookup } from 'lookup/tagColorLookup';
+    import {onMount} from 'svelte';
+    import {tagColorLookup, tagTextColorLookup} from 'lookup/tagColorLookup';
 
-	export let tag: TalkTag;
+    export let tag: TalkTag;
 
-	export let classes:string="";
+    export let classes: string = "";
 
-	onMount(() => {
-		const element = document.getElementsByClassName(`schedule-entry-tag-${tag.text}`);
+    onMount(() => {
+        const element = document.getElementsByClassName(`schedule-entry-tag-${tag.text}`);
 
-		Array.from(element).forEach((element) => {
-			(element as HTMLElement).style.backgroundColor = tagColorLookup(tag.color_index);
-			(element as HTMLElement).style.color = tagTextColorLookup(tag.color_index);
-		});
-	});
+        Array.from(element).forEach((element) => {
+            (element as HTMLElement).style.backgroundColor = tagColorLookup(tag.color_index);
+            (element as HTMLElement).style.color = tagTextColorLookup(tag.color_index);
+        });
+    });
 </script>
 
 <div class={classes}>
-	<TextLine classes="schedule-entry-tag-{tag.text} schedule-entry-tag">
-		{tag.text}
-	</TextLine>
+    <TextLine classes="schedule-entry-tag-{tag.text} schedule-entry-tag">
+        {tag.text}
+    </TextLine>
 </div>
 
 <style>
-	:global(.schedule-entry-tag) {
-		border-radius: var(--border-radius);
-		padding: var(--quad-padding) var(--half-padding);
-	}
+    :global(.schedule-entry-tag) {
+        border-radius: var(--border-radius);
+        padding:       var(--quad-padding) var(--half-padding);
+    }
 </style>
