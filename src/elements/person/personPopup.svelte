7f2b30ef6056f06f6870629e4502908d551cc12f
Martin <martin.schreurs@netcologne.de>
2024-12-24 13:34:30 +0100

    format person

--- src/elements/person/personPopup.svelte
+++ src/elements/person/personPopup.svelte
@@ -1,167 +1,173 @@
 <script lang="ts">
-	import type { Person } from 'types/provideTypes';
+    import type {Person} from 'types/provideTypes';
 
-	import Paragraph from 'elements/text/paragraph.svelte';
-	import SubHeadline from 'elements/text/subHeadline.svelte';
-	import PersonLinkGrid from './personLinkGrid.svelte';
-	import PersonImage from './personImage.svelte';
-	import Button from 'elements/input/button.svelte';
+    import Paragraph from 'elements/text/paragraph.svelte';
+    import SubHeadline from 'elements/text/subHeadline.svelte';
+    import PersonLinkGrid from './personLinkGrid.svelte';
+    import PersonImage from './personImage.svelte';
+    import Button from 'elements/input/button.svelte';
 
-	import { fade } from 'svelte/transition';
+    import {fade} from 'svelte/transition';
 
-	export let classes: string = '';
+    export let classes: string = '';
 
-	export let data: Person;
+    export let data: Person;
 </script>
 
-<dialog class={classes} transition:fade={{ duration: 300 }} role="presentation" on:click>
-	<div
-		class="person-popup-wrapper"
-		role="presentation"
-		on:click={(event) => {
+<dialog class={classes}
+        transition:fade={{ duration: 300 }}
+        role="presentation"
+        on:click>
+    <div
+          class="person-popup-wrapper"
+          role="presentation"
+          on:click={(event) => {
 			event.stopPropagation();
 		}}
-	>
-		<Button
-			fontSize="large-text"
-			classes="person-popup-close-button"
-			ariaLabel="close popup"
-			on:click>&times;</Button
-		>
-		<div class="person-popup-content-wrapper">
-			<div class="person-popup-column-wrapper person-popup-align-center person-popup-line">
-				<PersonImage classes="person-popup-picture" {data} />
-				<SubHeadline classes="person-popup-one-line-spacer">{data.name}</SubHeadline>
-				<Paragraph classes="person-popup-short-bio-paragraph paragraph-pre-wrap"
-					>{data.short_bio}</Paragraph
-				>
-				<PersonLinkGrid person={data.name} links={data.social_media_links} />
-			</div>
-			<div class="person-popup-column-wrapper">
-				<Paragraph classes="person-popup-one-line-spacer paragraph-pre-wrap">{data.bio}</Paragraph>
-			</div>
-		</div>
-	</div>
+    >
+        <Button
+              fontSize="large-text"
+              classes="person-popup-close-button"
+              ariaLabel="close popup"
+              on:click>&times;
+        </Button
+        >
+        <div class="person-popup-content-wrapper">
+            <div class="person-popup-column-wrapper person-popup-align-center person-popup-line">
+                <PersonImage classes="person-popup-picture"
+                             {data}/>
+                <SubHeadline classes="person-popup-one-line-spacer">{data.name}</SubHeadline>
+                <Paragraph classes="person-popup-short-bio-paragraph paragraph-pre-wrap"
+                >{data.short_bio}</Paragraph
+                >
+                <PersonLinkGrid person={data.name}
+                                links={data.social_media_links}/>
+            </div>
+            <div class="person-popup-column-wrapper">
+                <Paragraph classes="person-popup-one-line-spacer paragraph-pre-wrap">{data.bio}</Paragraph>
+            </div>
+        </div>
+    </div>
 </dialog>
 
 <style>
-	dialog {
-		display: flex;
-		position: fixed;
-		top: 0;
-		left: 0;
-		justify-content: center;
-		align-items: center;
-		background-color: var(--background-color-transparent);
-		width: 100%;
-		height: 100%;
-		z-index: 1000;
-	}
+    dialog {
+        display:          flex;
+        position:         fixed;
+        top:              0;
+        left:             0;
+        justify-content:  center;
+        align-items:      center;
+        background-color: var(--background-color-transparent);
+        width:            100%;
+        height:           100%;
+        z-index:          1000;
+    }
 
-	.person-popup-wrapper {
-		background-color: var(--background-color);
-		border: 1px solid var(--primary-color-light);
-		width: 90%;
-		max-width: calc(100rem - 2 * var(--4x-margin));
-		position: relative;
-		border-radius: var(--border-radius);
-	}
+    .person-popup-wrapper {
+        background-color: var(--background-color);
+        border:           1px solid var(--primary-color-light);
+        width:            90%;
+        max-width:        calc(100rem - 2 * var(--4x-margin));
+        position:         relative;
+        border-radius:    var(--border-radius);
+    }
 
-	.person-popup-content-wrapper {
-		margin: var(--full-margin);
-		display: flex;
-	}
+    .person-popup-content-wrapper {
+        margin:  var(--full-margin);
+        display: flex;
+    }
 
-	.person-popup-align-center {
-		text-align: center;
-	}
+    .person-popup-align-center {
+        text-align: center;
+    }
 
-	.person-popup-column-wrapper {
-		width: 50%;
-		height: fit-content;
-		display: flex;
-		flex-direction: column;
-		padding: var(--2x-margin);
-	}
+    .person-popup-column-wrapper {
+        width:          50%;
+        height:         fit-content;
+        display:        flex;
+        flex-direction: column;
+        padding:        var(--2x-margin);
+    }
 
-	:global(.person-popup-one-line-spacer) {
-		margin-top: var(--full-margin);
-	}
+    :global(.person-popup-one-line-spacer) {
+        margin-top: var(--full-margin);
+    }
 
-	:global(.person-popup-short-bio-paragraph) {
-		flex-grow: 1;
-	}
+    :global(.person-popup-short-bio-paragraph) {
+        flex-grow: 1;
+    }
 
-	.person-popup-line {
-		border-right: 1px solid var(--line-color);
-	}
+    .person-popup-line {
+        border-right: 1px solid var(--line-color);
+    }
 
-	:global(.person-popup-picture) {
-		width: auto;
-		height: auto;
-		max-width: 30rem;
-		border: 1px solid var(--primary-color-light);
-		align-self: center;
-	}
+    :global(.person-popup-picture) {
+        width:      auto;
+        height:     auto;
+        max-width:  30rem;
+        border:     1px solid var(--primary-color-light);
+        align-self: center;
+    }
 
-	:global(.person-popup-close-button) {
-		position: absolute;
-		top: 0;
-		right: 0;
-		translate: 50% -50%;
-		line-height: var(--2x-font-size);
-		padding: var(--half-padding);
-	}
+    :global(.person-popup-close-button) {
+        position:    absolute;
+        top:         0;
+        right:       0;
+        translate:   50% -50%;
+        line-height: var(--2x-font-size);
+        padding:     var(--half-padding);
+    }
 
-	.person-popup-close-picture {
-		height: 2.5rem;
-		width: 2.5rem;
-		margin: var(--quad-margin);
-	}
+    .person-popup-close-picture {
+        height: 2.5rem;
+        width:  2.5rem;
+        margin: var(--quad-margin);
+    }
 
-	@media (max-width: 1280px) {
-		:global(.person-popup-close-button) {
-			top: 100%;
-			right: 50%;
-		}
+    @media (max-width: 1280px) {
+        :global(.person-popup-close-button) {
+            top:   100%;
+            right: 50%;
+        }
 
-		:global(.person-popup-short-bio-paragraph) {
-			flex-grow: unset;
-		}
+        :global(.person-popup-short-bio-paragraph) {
+            flex-grow: unset;
+        }
 
-		:global(.person-popup-picture) {
-			max-width: 20rem;
-		}
-	}
+        :global(.person-popup-picture) {
+            max-width: 20rem;
+        }
+    }
 
-	@media (max-width: 900px) {
-		.person-popup-wrapper {
-			height: calc(100vh - 8rem);
-		}
+    @media (max-width: 900px) {
+        .person-popup-wrapper {
+            height: calc(100vh - 8rem);
+        }
 
-		.person-popup-content-wrapper {
-			flex-direction: column;
-			overflow: scroll;
-			height: 95%;
-		}
+        .person-popup-content-wrapper {
+            flex-direction: column;
+            overflow:       scroll;
+            height:         95%;
+        }
 
-		.person-popup-column-wrapper {
-			width: 100%;
-			height: auto;
-		}
+        .person-popup-column-wrapper {
+            width:  100%;
+            height: auto;
+        }
 
-		:global(.person-popup-picture) {
-			max-width: 20rem;
-			align-self: center;
-		}
+        :global(.person-popup-picture) {
+            max-width:  20rem;
+            align-self: center;
+        }
 
-		:global(.person-popup-close-button) {
-			top: 102%;
-		}
+        :global(.person-popup-close-button) {
+            top: 102%;
+        }
 
-		.person-popup-line {
-			border-bottom: 1px solid var(--line-color);
-			border-right: none;
-		}
-	}
+        .person-popup-line {
+            border-bottom: 1px solid var(--line-color);
+            border-right:  none;
+        }
+    }
 </style>
--- src/elements/person/personRectangle.svelte
+++ src/elements/person/personRectangle.svelte
@@ -1,89 +1,96 @@
 <script lang="ts">
-	import type { Person } from 'types/provideTypes';
+    import type {Person} from 'types/provideTypes';
 
-	import SubHeadline from 'elements/text/subHeadline.svelte';
-	import Paragraph from 'elements/text/paragraph.svelte';
-	import LinkArray from './personLinkGrid.svelte';
-	import Button from 'elements/input/button.svelte';
-	import PersonImage from './personImage.svelte';
+    import SubHeadline from 'elements/text/subHeadline.svelte';
+    import Paragraph from 'elements/text/paragraph.svelte';
+    import LinkArray from './personLinkGrid.svelte';
+    import Button from 'elements/input/button.svelte';
+    import PersonImage from './personImage.svelte';
 
-	export let classes: string = '';
+    export let classes: string = '';
 
-	export let data: Person;
+    export let data: Person;
 
-	function onLinkClicked(event: Event) {
-		event.stopPropagation();
-	}
+    function onLinkClicked(event: Event) {
+        event.stopPropagation();
+    }
 </script>
 
-<div class="person-rectangle {classes}" on:click role="presentation">
-	<PersonImage classes="person-rectangle-picture" {data} />
-	<div class="person-rectangle-text-wrapper">
-		<SubHeadline>{data.name}</SubHeadline>
-		<Paragraph classes="person-rectangle-paragraph">{data.short_bio}</Paragraph>
-	</div>
-	<div class="person-rectangle-flex-growth" />
-	<div class="person-rectangle-bottom-wrapper">
-		<LinkArray on:click={onLinkClicked} person={data.name} links={data.social_media_links} />
-	</div>
-	<Button classes="person-rectangle-button" on:click ariaLabel="Mehr Informationen zu {data.name}">
-		Info
-	</Button>
+<div class="person-rectangle {classes}"
+     on:click
+     role="presentation">
+    <PersonImage classes="person-rectangle-picture"
+                 {data}/>
+    <div class="person-rectangle-text-wrapper">
+        <SubHeadline>{data.name}</SubHeadline>
+        <Paragraph classes="person-rectangle-paragraph">{data.short_bio}</Paragraph>
+    </div>
+    <div class="person-rectangle-flex-growth"/>
+    <div class="person-rectangle-bottom-wrapper">
+        <LinkArray on:click={onLinkClicked}
+                   person={data.name}
+                   links={data.social_media_links}/>
+    </div>
+    <Button classes="person-rectangle-button"
+            on:click
+            ariaLabel="Mehr Informationen zu {data.name}">
+        Info
+    </Button>
 </div>
 
 <style>
-	.person-rectangle {
-		display: flex;
-		flex-direction: column;
-		min-width: 15rem;
-		border: none;
-		background-color: transparent;
-		outline: 1px solid var(--primary-color-light);
-		border-radius: var(--border-radius);
-		transition: var(--transition-duration);
-		text-align: center;
-	}
+    .person-rectangle {
+        display:          flex;
+        flex-direction:   column;
+        min-width:        15rem;
+        border:           none;
+        background-color: transparent;
+        outline:          1px solid var(--primary-color-light);
+        border-radius:    var(--border-radius);
+        transition:       var(--transition-duration);
+        text-align:       center;
+    }
 
-	.person-rectangle:hover {
-		outline: 1px solid var(--outline-hover-color);
-		transition: var(--transition-duration);
-	}
+    .person-rectangle:hover {
+        outline:    1px solid var(--outline-hover-color);
+        transition: var(--transition-duration);
+    }
 
-	:global(.person-rectangle-picture) {
-		width: 100%;
-		height: auto;
-		background-color: var(--primary-color-light);
-		border-top-left-radius: var(--border-radius);
-		border-top-right-radius: var(--border-radius);
-		margin-bottom: var(--full-margin);
-	}
+    :global(.person-rectangle-picture) {
+        width:                   100%;
+        height:                  auto;
+        background-color:        var(--primary-color-light);
+        border-top-left-radius:  var(--border-radius);
+        border-top-right-radius: var(--border-radius);
+        margin-bottom:           var(--full-margin);
+    }
 
-	.person-rectangle-text-wrapper {
-		height: 11rem;
-		margin: 0 var(--half-margin);
-	}
+    .person-rectangle-text-wrapper {
+        height: 11rem;
+        margin: 0 var(--half-margin);
+    }
 
-	:global(.person-rectangle-paragraph) {
-		margin-top: var(--half-margin);
-		display: -webkit-box;
-		-webkit-line-clamp: 3;
-		-webkit-box-orient: vertical;
-		overflow: hidden;
-	}
+    :global(.person-rectangle-paragraph) {
+        margin-top:         var(--half-margin);
+        display:            -webkit-box;
+        -webkit-line-clamp: 3;
+        -webkit-box-orient: vertical;
+        overflow:           hidden;
+    }
 
-	.person-rectangle-flex-growth {
-		flex-grow: 1;
-		margin: var(--2x-margin) var(--2x-margin) 0;
-		border-top: 1px solid var(--line-color);
-	}
+    .person-rectangle-flex-growth {
+        flex-grow:  1;
+        margin:     var(--2x-margin) var(--2x-margin) 0;
+        border-top: 1px solid var(--line-color);
+    }
 
-	.person-rectangle-bottom-wrapper {
-		margin: var(--2x-margin) var(--2x-margin) 0;
-	}
+    .person-rectangle-bottom-wrapper {
+        margin: var(--2x-margin) var(--2x-margin) 0;
+    }
 
-	:global(.person-rectangle-button) {
-		display: block;
-		padding: var(--quad-padding) var(--2x-padding) !important;
-		margin: 0 var(--2x-margin) var(--2x-margin) var(--2x-margin) !important;
-	}
+    :global(.person-rectangle-button) {
+        display: block;
+        padding: var(--quad-padding) var(--2x-padding) !important;
+        margin:  0 var(--2x-margin) var(--2x-margin) var(--2x-margin) !important;
+    }
 </style>
