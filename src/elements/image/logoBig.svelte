eea544a7bd884bfa353d88d987d980c83760a75a
Martin <martin.schreurs@netcologne.de>
2024-12-24 13:27:13 +0100

    format images

--- src/elements/image/logoBig.svelte
+++ src/elements/image/logoBig.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import Image from "./image.svelte";
+    import Image from "./image.svelte";
 
-	export let classes: string = '';
+    export let classes: string = '';
 </script>
 
 <Image
-	{classes}
-	src={'/logo_big.png'}
-	alt="Das große Tech Stream Conference Logo. Es zeigt den Schriftzug auf schwarzen Wolken. Aus den Wolken kommen schwarze Schaltkreise, an deren Enden gelbe Punkte sind. Test ist in Großbuchstaben und weiß / grün. conf in Kleinbuchstaben darunter und weiß."
+      {classes}
+      src={'/logo_big.png'}
+      alt="Das große Tech Stream Conference Logo. Es zeigt den Schriftzug auf schwarzen Wolken. Aus den Wolken kommen schwarze Schaltkreise, an deren Enden gelbe Punkte sind. Test ist in Großbuchstaben und weiß / grün. conf in Kleinbuchstaben darunter und weiß."
 />
