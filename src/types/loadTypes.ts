bc10d3fa60bc17cfb39f437456342024b190759e
Martin <martin.schreurs@netcologne.de>
2024-12-26 13:55:03 +0100

    format load types

--- src/types/loadTypes.ts
+++ src/types/loadTypes.ts
@@ -1,11 +1,28 @@
-import type { Year, Globals } from "./provideTypes";
+import type { Year, Globals } from './provideTypes';
 
-export type LoadYear = { loggedIn: boolean; globals: Globals; year: Year };
+export type LoadYear = {
+    loggedIn: boolean;
+    globals: Globals;
+    year: Year
+};
 
-export type LoadRegister = { loggedIn: boolean; globals: Globals };
+export type LoadRegister = {
+    loggedIn: boolean;
+    globals: Globals
+};
 
-export type LoadLogin = { loggedIn: boolean; globals: Globals; showLoginMessage: boolean; };
+export type LoadLogin = {
+    loggedIn: boolean;
+    globals: Globals;
+    showLoginMessage: boolean;
+};
 
-export type LoadImprint = { loggedIn: boolean; globals: Globals; };
+export type LoadImprint = {
+    loggedIn: boolean;
+    globals: Globals;
+};
 
-export type LoadVerifyEmail = { success: boolean | undefined, globals: Globals; };
+export type LoadVerifyEmail = {
+    success: boolean | undefined,
+    globals: Globals;
+};
