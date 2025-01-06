f55fbc7f00554837d365ae47cf2c7d0d8f1b4469
Martin <martin.schreurs@netcologne.de>
2025-01-06 21:43:48 +0100

    feat: state logic for password reset

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -9,6 +9,7 @@
     import Input from 'elements/input/input.svelte';
     import Button from 'elements/input/button.svelte';
     import Link from 'elements/text/link.svelte';
+    import Paragraph from 'elements/text/paragraph.svelte';
 
     import { goto } from '$app/navigation';
     import { apiUrl } from 'helper/links';
@@ -16,12 +17,25 @@
 
     export let data: LoadLogin; // data from database
 
+    enum State {
+        Login,
+        ResetPasswordPending,
+        ResetPasswordSuccess,
+    }
+
+    let state: State = State.Login;
+
     let usernameOrEmail: string = '';
     let password: string        = '';
 
     const loggedInMessage     = data.loggedIn ? 'Du bist bereits angemeldet.' : '';
     const displayLoginMessage = data.showLoginMessage ? 'Du musst dich zunächst anmelden.' : '';
     let errorMessage: string  = '';
+
+    function changeState(_state: State): void {
+        errorMessage = '';
+        state        = _state;
+    }
 
     async function loginAsync(): Promise<void> {
         const data = {
@@ -48,47 +62,95 @@
         };
         errorMessage       = await entriesAsync(response);
     }
+
+    async function resetPassword(): Promise<void> {
+        changeState(State.ResetPasswordSuccess);
+    }
 </script>
 
 <Header menu={Menu.headerOut} />
 
 <div class="login">
     <div class="login-content">
-        <form class="login-form-width-wrapper"
-              on:submit|preventDefault={loginAsync}>
-            <HeadlinePage>Anmelden</HeadlinePage>
-            <div class="login-message-wrapper">
-                <ErrorMessage message={loggedInMessage} />
-                <ErrorMessage message={displayLoginMessage} />
+        {#if state === State.Login}
+            <form class="login-form-width-wrapper"
+                  on:submit|preventDefault={loginAsync}>
+                <HeadlinePage>Anmelden</HeadlinePage>
+                <div class="login-message-wrapper">
+                    <ErrorMessage message={loggedInMessage} />
+                    <ErrorMessage message={displayLoginMessage} />
+                    <ErrorMessage message={errorMessage} />
+                </div>
+                <Input
+                      classes="login-username-mail"
+                      id="login-username-or-email"
+                      type="text"
+                      labelText="Nutzername oder E-Mail:"
+                      placeholderText="Nutzername oder E-Mail"
+                      ariaLabel="Gib den Nutzernamen oder die E-Mail ein"
+                      bind:value={usernameOrEmail}
+                />
+                <Input
+                      classes="login-password"
+                      id="login-password"
+                      type="password"
+                      labelText="Passwort:"
+                      placeholderText="Passwort"
+                      ariaLabel="Gib das Passwort ein"
+                      bind:value={password}
+                />
+                <Link href="/register"
+                      title="Klicke, um einen neuen Account anzulegen">
+                    Noch keinen Account?
+                </Link>
+                <Link href="#"
+                      title="Klicke, um dein Passwort zurück zu setzen."
+                      on:click={() => {changeState(State.ResetPasswordPending)}}
+                >
+                    Passwort vergessen?
+                </Link>
+                <Button classes="login-button"
+                        type="submit"
+                        ariaLabel="Klicke zum Anmelden">Anmelden
+                </Button>
+            </form>
+        {:else if state === State.ResetPasswordPending}
+            <form class="login-form-width-wrapper"
+                  on:submit|preventDefault={resetPassword}>
+                <HeadlinePage>Passwort zurücksetzen</HeadlinePage>
                 <ErrorMessage message={errorMessage} />
-            </div>
-            <Input
-                  classes="login-username-mail"
-                  id="login-username-or-email"
-                  type="text"
-                  labelText="Nutzername oder E-Mail:"
-                  placeholderText="Nutzername oder E-Mail"
-                  ariaLabel="Gib den Nutzernamen oder die E-Mail ein"
-                  bind:value={usernameOrEmail}
-            />
-            <Input
-                  classes="login-password"
-                  id="login-password"
-                  type="password"
-                  labelText="Passwort:"
-                  placeholderText="Passwort"
-                  ariaLabel="Gib das Passwort ein"
-                  bind:value={password}
-            />
-            <Link href="/register"
-                  title="Klicke, um einen neuen Account anzulegen">
-                Noch keinen Account?
-            </Link>
-            <Button classes="login-button"
-                    type="submit"
-                    ariaLabel="Klicke zum Anmelden">Anmelden
-            </Button>
-        </form>
+                <Input
+                      classes="login-username-mail login-username-mail-reset-extra"
+                      id="login-username-or-email"
+                      type="text"
+                      labelText="Nutzername oder E-Mail:"
+                      placeholderText="Nutzername oder E-Mail"
+                      ariaLabel="Gib den Nutzernamen oder die E-Mail ein"
+                      bind:value={usernameOrEmail}
+                />
+                <Button classes="login-button"
+                        type="submit"
+                        ariaLabel="Klicke, um dein Passwort zurückzusetzen">Passwort zurücksetzen
+                </Button>
+                <Link href="#"
+                      title="Klicke, wieder zum login zu kommen."
+                      on:click={() => {changeState(State.Login)}}
+                >
+                    zurück
+                </Link>
+            </form>
+        {:else if state === State.ResetPasswordSuccess}
+            <form class="login-form-width-wrapper login-form-gap-wrapper"
+                  on:submit|preventDefault={() => {changeState(State.Login);}}>
+                <HeadlinePage>Passwort zurückgesetzt</HeadlinePage>
+                <Paragraph classes="paragraph-pre-wrap"
+                           --text-align="center">{'Dein Passwort wurde erfolgreich zurück gesetzt.\nDu erhältst eine Mail mit einem Link.\nDort kannst du dein neues Passwort eingeben.\nDanach kannst du dich mit deinem neuen Passwort wieder anmelden.'}</Paragraph>
+                <Button classes="login-button"
+                        type="submit"
+                        ariaLabel="Klicke, um dich anzumelden">Login
+                </Button>
+            </form>
+        {/if}
     </div>
 
     <Footer globals={data.globals}
@@ -117,6 +179,10 @@
         flex-direction: column;
     }
 
+    .login-form-gap-wrapper {
+        gap: var(--4x-gap);
+    }
+
     .login-message-wrapper {
         display:        flex;
         flex-direction: column;
@@ -125,6 +191,10 @@
 
     :global(.login-username-mail) {
         margin-bottom: var(--full-margin);
+    }
+
+    :global(.login-username-mail-reset-extra) {
+        margin-top: var(--4x-margin);
     }
 
     :global(.login-password) {
