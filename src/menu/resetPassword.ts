801ba9a48d9c33c21d3574d52ac05c61b90af27c
Martin <martin.schreurs@netcologne.de>
2025-01-08 13:08:31 +0100

    feat: add reset password menus

---
+++ src/menu/resetPassword.ts
@@ -0,0 +1,12 @@
+import type { Menu } from 'types/provideTypes';
+import * as Item from './menuItems';
+
+export const header: Menu = [
+    Item.mainPage,
+    Item.login,
+];
+
+export const footer: Menu = [
+    Item.mainPage,
+    Item.login,
+];
