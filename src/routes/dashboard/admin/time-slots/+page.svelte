c5d461ad4cd904a8c9ed363de38f32a0c80e1680
Martin <martin.schreurs@netcologne.de>
2025-01-09 15:04:50 +0100

    feat: page navigation

---
+++ src/routes/dashboard/admin/time-slots/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import * as Menu from 'menu/dashboard';
+    import * as MenuItem from 'menu/menuItems';
+
+    import type { LoadAdminTimeSlots } from 'types/dashboardLoadTypes';
+
+    import Tabs from 'elements/navigation/tabs.svelte';
+    import SectionDashboard from 'elements/section/sectionDashboard.svelte';
+
+    export let data: LoadAdminTimeSlots;
+</script>
+
+<SectionDashboard classes="standard-dashboard-section">
+    <Tabs classes="navigation-tabs-dashboard-subpage"
+          entries={Menu.admin}
+          entryName={MenuItem.adminEventSlots.name} />
+</SectionDashboard>
