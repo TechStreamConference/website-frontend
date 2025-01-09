426ee16b39824bd19de0b27db577d0c890af3d1f
Martin <martin.schreurs@netcologne.de>
2025-01-09 16:06:23 +0100

    feat: use navigation drop down in time slots page

--- src/routes/dashboard/admin/time-slots/+page.svelte
+++ src/routes/dashboard/admin/time-slots/+page.svelte
@@ -4,14 +4,38 @@
 
     import type { LoadAdminTimeSlots } from 'types/dashboardLoadTypes';
 
+    import { setUnsavedChanges } from 'stores/saved';
+
     import Tabs from 'elements/navigation/tabs.svelte';
     import SectionDashboard from 'elements/section/sectionDashboard.svelte';
+    import NavigationDropDown from 'elements/navigation/NavigationDropDown.svelte';
+    import Button from 'elements/input/button.svelte';
+    import UnsavedChangesCallbackWrapper from 'elements/navigation/unsavedChangesCallbackWrapper.svelte';
 
     export let data: LoadAdminTimeSlots;
+
+    function updateDisplayed(value: string) {
+        console.log(value);
+    }
+
+    async function save(): Promise<boolean> {
+        console.log('save');
+        return true;
+    }
 </script>
 
+<UnsavedChangesCallbackWrapper callback={save} />
 <SectionDashboard classes="standard-dashboard-section">
     <Tabs classes="navigation-tabs-dashboard-subpage"
           entries={Menu.admin}
           entryName={MenuItem.adminEventSlots.name} />
+    <NavigationDropDown id="dashboard-admin-time-slots-event-drop-down"
+                        labelText="Aktuelles Event:"
+                        data={data.allEvents.map(x=>x.title)}
+                        on:navigated={(e) => {updateDisplayed(e.detail);}} />
+
+    <Button ariaLabel=""
+            on:click={setUnsavedChanges}>TEST
+    </Button>
+
 </SectionDashboard>
