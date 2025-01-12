87137c681f83ea871f74986c1b6eecbd02716ced
Martin <martin.schreurs@netcologne.de>
2025-01-12 16:00:24 +0100

    fix: make toggle abstract

--- src/elements/input/toggle.svelte
+++ src/elements/input/toggle.svelte
@@ -1,29 +1,30 @@
 <script lang="ts">
-    import Icon from 'elements/image/icon.svelte';
-
     export let toggle: boolean;
     export let ariaLabel: string;
-    export let iconTrue: string;
-    export let iconFalse: string;
+
+    export let classes: string = '';
 
     function onToggle() {
         toggle = !toggle;
     }
 </script>
 
-<button class="toggle-button"
+<button class="toggle-button {classes}"
         type="button"
         aria-label="{ariaLabel}"
         on:click={onToggle}
 >
-    <Icon type={toggle ? iconTrue : iconFalse}
-          color={toggle ? "green" : "red" } />
+    {#if toggle}
+        <slot name="slotTrue" />
+    {:else}
+        <slot name="slotFalse" />
+    {/if}
 </button>
 
 <style>
     .toggle-button {
         background: none;
         border:     none;
-        cursor:     pointer;
+        cursor: pointer;
     }
 </style>
