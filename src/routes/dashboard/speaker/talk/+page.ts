11f8fa9c57e7d39f69dfd02826396afbae9385ad
Martin <martin.schreurs@netcologne.de>
2025-01-15 23:32:42 +0100

    fix: remove test values

--- src/routes/dashboard/speaker/talk/+page.ts
+++ src/routes/dashboard/speaker/talk/+page.ts
@@ -46,10 +46,6 @@ export async function load({ fetch }: {
     const allTalks        = await loadTalkFromEventIDAsync(fetch, eventID);
     const currentTalk     = allTalks.length > 0 ? new Clone(allTalks[0]) : undefined;
 
-    if (currentTalk) {
-        currentTalk.value.time_slot_accepted = false;
-    }
-
     return {
         allEvents,
         allTalks,
