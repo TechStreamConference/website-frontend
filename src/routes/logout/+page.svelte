c184908739989cd7f47f4a91e8ff743ea858f8db
Martin <martin.schreurs@netcologne.de>
2025-01-23 22:13:50 +0100

    feat: logout

--- src/routes/logout/+page.svelte
+++ src/routes/logout/+page.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
     import { goto } from '$app/navigation';
-    import { apiUrl } from 'helper/links';
     import { onMount } from 'svelte';
+
+    import { trySaveDashboardDataAsync } from 'helper/trySaveDashboardData';
+
     import TextLine from 'elements/text/textLine.svelte';
+    import MessageWrapper from 'elements/text/messageWrapper.svelte';
+
+    let errorQueue: string[] = [];
 
     async function logoutAsync(): Promise<void> {
-        const response: Response = await fetch(apiUrl('/api/account/logout'), { method: 'POST' });
-        if (!response.ok) {
+        const result = await trySaveDashboardDataAsync([], '/api/account/logout', 'POST');
+        if (!result.success) {
             return;
         }
         await goto('/');
@@ -18,3 +23,4 @@
 </script>
 
 <TextLine>Logging out...</TextLine>
+<MessageWrapper messages={errorQueue} />
