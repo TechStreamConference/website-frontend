ade2c1dcff3fffbb9bc6cb1c196fa9f700789cff
Martin <martin.schreurs@netcologne.de>
2025-01-23 21:04:00 +0100

    feat: team member

--- src/routes/dashboard/team-member/social-media/+page.svelte
+++ src/routes/dashboard/team-member/social-media/+page.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
     import SpeakerTeamMemberSocialMediaLinks from 'pages/speakerTeamMemberSocialMediaLinks.svelte';
-    import type {
-        LoadDashboard,
-        LoadUserSocials,
-    } from 'types/dashboardLoadTypes';
+    import type { LoadDashboard, LoadUserSocials } from 'types/dashboardLoadTypes';
 
     export let data: LoadDashboard & LoadUserSocials;
 </script>
