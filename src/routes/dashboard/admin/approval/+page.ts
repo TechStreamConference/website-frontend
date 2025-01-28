e22a6606546105112fec26a5435aede5dbdeaaec
Martin <martin.schreurs@netcologne.de>
2025-01-28 21:26:25 +0100

    feat: load reject speaker

--- src/routes/dashboard/admin/approval/+page.ts
+++ src/routes/dashboard/admin/approval/+page.ts
@@ -1,11 +1,12 @@
 import type { LoadAdminApproval } from 'types/dashboardLoadTypes';
 
 import { apiUrl } from 'helper/links';
-import { checkAndParseInputDataAsync } from 'helper/parseJson';
+import { checkAndParseInputDataAsync, parseProvidedJsonAsync } from 'helper/parseJson';
 import {
-    dashboardAllApprovalSocialMediaLinkScheme, dashboardAllApprovalSpeakerTeamMemberScheme,
+    dashboardAllApprovalSocialMediaLinkScheme,
+    dashboardAllApprovalSpeakerTeamMemberScheme,
 } from 'types/dashboardProvideTypes';
-
+import { z } from 'zod';
 
 export async function load({ fetch }: {
     fetch: typeof globalThis.fetch
@@ -13,7 +14,6 @@ export async function load({ fetch }: {
     const speakerFetchPromise: Promise<Response>     = fetch(apiUrl('/api/dashboard/admin/approval/speaker'));
     const teamMemberFetchPromise: Promise<Response>  = fetch(apiUrl('/api/dashboard/admin/approval/team-member'));
     const socialMediaFetchPromise: Promise<Response> = fetch(apiUrl('/api/dashboard/admin/approval/social-media-link'));
-
 
     const speakerParsePromise     = checkAndParseInputDataAsync(
         await speakerFetchPromise,
@@ -34,8 +34,30 @@ export async function load({ fetch }: {
         `Unerwartete Daten für die Freigabedaten der Social Media Link.`,
     );
 
+    const speaker = await speakerParsePromise;
+
+    const promises: Promise<Response>[] = [];
+    for (const entry of speaker) {
+        const promise = fetch(apiUrl(`/api/dashboard/admin/approval/user/${entry.user_id}/event/${entry.event_id}/can-reject`));
+        promises.push(promise);
+    }
+
+    for (let i = 0; i < promises.length; ++i) {
+        const response = await promises[i];
+        if (!response.ok) {
+            speaker[i].can_reject = false;
+            continue;
+        }
+
+        const result          = await parseProvidedJsonAsync(response, z.object({
+                                                                                    can_reject: z.boolean(),
+                                                                                }));
+        speaker[i].can_reject = result ? result.can_reject : false;
+        // speaker[i].can_reject = true;
+    }
+
     return {
-        speaker:     await speakerParsePromise,
+        speaker,
         teamMember:  await teamMemberParsePromise,
         socialMedia: await socialMediaParsePromise,
     };
