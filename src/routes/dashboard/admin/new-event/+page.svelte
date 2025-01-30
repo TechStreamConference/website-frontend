dd1a7dea24e73fe26916a32042029e3a6868f3d5
Martin <martin.schreurs@netcologne.de>
2025-01-30 22:51:15 +0100

    feat: provide a new event page

---
+++ src/routes/dashboard/admin/new-event/+page.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+    import * as Menu from 'menu/dashboard';
+    import * as MenuItem from 'menu/menuItems';
+
+    import { type DashboardEvent } from 'types/dashboardProvideTypes';
+
+    import { formatDate } from 'helper/dates';
+
+    import Tabs from 'elements/navigation/tabs.svelte';
+    import SectionDashboard from 'elements/section/sectionDashboard.svelte';
+    import AdminEventForm from 'forms/adminEventForm.svelte';
+    import TextLine from 'elements/text/textLine.svelte';
+    import StyledLink from 'elements/input/styledLink.svelte';
+    import { adminEvents } from 'menu/menuItems';
+
+    enum State {
+        Default,
+        Saved,
+    }
+
+    let currentState: State = State.Default;
+
+    let event: DashboardEvent = {
+        id:                    0,
+        title:                 '',
+        subtitle:              '',
+        start_date:            formatDate(String(new Date()), '%YYYY-%MM-%DD'),
+        end_date:              formatDate(String(new Date()), '%YYYY-%MM-%DD'),
+        discord_url:           '',
+        twitch_url:            '',
+        presskit_url:          '',
+        trailer_youtube_id:    '',
+        description_headline:  '',
+        description:           '',
+        schedule_visible_from: formatDate(String(new Date()), '%YYYY-%MM-%DDT%hh:00:00'),
+        publish_date:          formatDate(String(new Date()), '%YYYY-%MM-%DDT%hh:00:00'),
+        call_for_papers_start: formatDate(String(new Date()), '%YYYY-%MM-%DDT%hh:00:00'),
+        call_for_papers_end:   formatDate(String(new Date()), '%YYYY-%MM-%DDT%hh:00:00'),
+    };
+
+</script>
+
+<Tabs entries={Menu.admin}
+      entryName={MenuItem.adminNewEvent.name}
+      classes="navigation-tabs-dashboard-subpage" />
+
+<SectionDashboard classes="dashboard-admin-event-section">
+    {#if currentState === State.Saved}
+        <div class="dashboard-admin-new-event-message-wrapper">
+            <TextLine>Das event '{event.title}' wurde erfolgreich angelegt.</TextLine>
+            <TextLine>Du kannst es ab jetzt im Event Tab bearbeiten.</TextLine>
+            <StyledLink text={MenuItem.adminEvents.name}
+                        title={adminEvents.description}
+                        href={adminEvents.url}
+                        newTab={false} />
+        </div>
+    {:else}
+        <AdminEventForm {event}
+                        speakerArray={undefined}
+                        on:save={() => currentState = State.Saved} />
+    {/if}
+</SectionDashboard>
+
+<style>
+    .dashboard-admin-new-event-message-wrapper {
+        display:        flex;
+        flex-direction: column;
+        align-items:    center;
+        margin-top:     var(--4x-margin);
+        gap:            var(--full-gap);
+    }
+</style>
