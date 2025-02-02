45a9e8340a7fddc9172a22c09cabf0acdf0f22db
Martin <martin.schreurs@netcologne.de>
2025-02-02 17:20:52 +0100

    feat: pending talk logic

---
+++ src/routes/dashboard/admin/approval-talk/+page.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+    import * as Menu from 'menu/dashboard';
+    import * as MenuItem from 'menu/menuItems';
+
+    import type { LoadAdminApprovalTalk } from 'types/dashboardLoadTypes';
+
+    import { fetchTentativeTalks, getUserIds } from './approvalHelper';
+
+    import Tabs from 'elements/navigation/tabs.svelte';
+    import SectionDashboard from 'elements/section/sectionDashboard.svelte';
+    import AdminPendingTalkApprovalFormWrapper from 'forms/adminPendingTalkApprovalFormWrapper.svelte';
+    import SubHeadline from 'elements/text/subHeadline.svelte';
+
+    export let data: LoadAdminApprovalTalk;
+
+    enum DataType {
+        Pending,
+        Tentative,
+    }
+
+    function getElementsByUserID<T extends {
+        speaker: {
+            user_id: number
+        }
+    }>(elements: T[], userID: number): T[] {
+        return elements.filter((entry) => entry.speaker.user_id === userID);
+    }
+
+    function deleteEntry(type: DataType, id: number): void {
+        setTimeout(() => {
+            switch (type) {
+                case DataType.Pending:
+                    data.pendingTalks = data.pendingTalks.filter((talk) => talk.id !== id);
+                    break;
+                case DataType.Tentative:
+                    data.tentativeTalks = data.tentativeTalks.filter((talk) => talk.id !== id);
+                    break;
+            }
+            data.userIDArray = getUserIds(data.pendingTalks, data.tentativeTalks);
+        }, 3000);
+    }
+
+    async function moveTalk(id: number): Promise<void> {
+        data.pendingTalks   = data.pendingTalks.filter((talk) => talk.id !== id);
+        data.tentativeTalks = await fetchTentativeTalks(fetch);
+        data.userIDArray    = getUserIds(data.pendingTalks, data.tentativeTalks);
+    }
+</script>
+
+<Tabs classes="navigation-tabs-dashboard-subpage"
+      entries={Menu.admin}
+      entryName={MenuItem.adminApprovalTalk.name} />
+
+<SectionDashboard classes="standard-dashboard-section dashboard-admin-talk-approval-section">
+    {#each data.userIDArray as userID}
+        <div class="dashboard-admin-talk-approval-wrapper">
+            <SubHeadline classes="sub-headline-center">{userID}</SubHeadline>
+            <AdminPendingTalkApprovalFormWrapper talks={getElementsByUserID(data.pendingTalks, userID)}
+                                                 on:reject={(e) => deleteEntry(DataType.Pending, e.detail)}
+                                                 on:approve={(e) => moveTalk(e.detail)} />
+        </div>
+    {/each}
+</SectionDashboard>
+
+<style>
+    :global(.dashboard-admin-talk-approval-section) {
+        gap: var(--4x-gap) !important;
+    }
+
+    .dashboard-admin-talk-approval-wrapper {
+        display:        flex;
+        flex-direction: column;
+        gap:            var(--4x-gap);
+        border:         1px solid var(--primary-color-dark);
+        border-radius:  var(--border-radius);
+        padding:        var(--full-padding);
+    }
+</style>
