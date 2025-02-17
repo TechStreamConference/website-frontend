9656c98b0b571af53ae3a6f4c5929da52e79b2d1
Martin <martin.schreurs@netcologne.de>
2025-02-17 20:27:39 +0100

    feat: first dummy page

---
+++ src/routes/dashboard/admin/create-panel-discussion/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import * as Menu from 'menu/dashboard';
+    import * as MenuItem from 'menu/menuItems';
+
+    import type { LoadAdminCreatePanelDiscussion } from 'types/dashboardLoadTypes';
+
+    import Tabs from 'elements/navigation/tabs.svelte';
+    import SectionDashboard from 'elements/section/sectionDashboard.svelte';
+
+    export let data: LoadAdminCreatePanelDiscussion;
+</script>
+
+<Tabs classes="navigation-tabs-dashboard-subpage"
+      entries={Menu.admin}
+      entryName={MenuItem.adminCreatePanelDiscussion.name} />
+
+<SectionDashboard classes="standard-dashboard-section">
+
+</SectionDashboard>
+
+
+<style>
+
+</style>
