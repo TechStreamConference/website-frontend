ad461dce10dc7ae5e46475c9a2e2104d92250da0
Martin <martin.schreurs@netcologne.de>
2025-02-17 21:34:58 +0100

    feat: add event form to page

--- src/routes/dashboard/admin/create-panel-discussion/+page.svelte
+++ src/routes/dashboard/admin/create-panel-discussion/+page.svelte
@@ -3,17 +3,36 @@
     import * as MenuItem from 'menu/menuItems';
 
     import type { LoadAdminCreatePanelDiscussion } from 'types/dashboardLoadTypes';
+    import type { DashboardAllPersons, DashboardPendingTalk } from 'types/dashboardProvideTypes';
+
+    import { getDropDownEntriesWithID, getIdFromDropDownEntry } from 'helper/navigation';
+    import { error } from '@sveltejs/kit';
+    import { initializePendingTalk, loadPossibleHosts } from './panelHelper';
+    import { onMount } from 'svelte';
 
     import Tabs from 'elements/navigation/tabs.svelte';
     import SectionDashboard from 'elements/section/sectionDashboard.svelte';
     import NavigationDropDown from 'elements/navigation/navigationDropDown.svelte';
-    import { getDropDownEntriesWithID, getIdFromDropDownEntry } from 'helper/navigation';
-    import { error } from '@sveltejs/kit';
-    import { loadPossibleHosts } from './panelHelper';
+    import SpeakerTalkForm from 'forms/speakerTalkForm.svelte';
+    import PersonArray from 'elements/input/personArray.svelte';
+    import Button from 'elements/input/button.svelte';
 
     export let data: LoadAdminCreatePanelDiscussion;
 
-    let currentEventId: number = 0;
+    let talkData: DashboardPendingTalk     = initializePendingTalk();
+    let selectedHosts: DashboardAllPersons = [];
+    let currentEventId: number             = 0;
+
+    let eventDropDown: NavigationDropDown;
+
+    onMount(() => {
+        const entry = eventDropDown.getSelected();
+        if (typeof entry !== 'string') {
+            console.error(`selected entry is not a string: '${entry}'`);
+            return;
+        }
+        currentEventId = getIdFromDropDownEntry(entry);
+    });
 
     async function onNavigate(dropDownEntry: string): Promise<void> {
         currentEventId = getIdFromDropDownEntry(dropDownEntry);
@@ -22,6 +41,14 @@
         }
 
         data.possibleHosts = await loadPossibleHosts(fetch, currentEventId);
+        talkData           = initializePendingTalk();
+    }
+
+    async function save(): Promise<boolean> {
+        console.log(currentEventId);
+        console.log(talkData);
+        console.log(selectedHosts);
+        return false;
     }
 </script>
 
@@ -30,13 +57,37 @@
       entryName={MenuItem.adminCreatePanelDiscussion.name} />
 
 <SectionDashboard classes="standard-dashboard-section">
-    <NavigationDropDown labelText="Aktuelles Jahr:"
+    <NavigationDropDown bind:this={eventDropDown}
+                        labelText="Aktuelles Jahr:"
                         id="dashboard-admin-create-panel-discussion-event-drop-down"
                         data={getDropDownEntriesWithID(data.allEvents)}
                         on:navigated={(e) => {onNavigate(e.detail)}} />
+    <form class="dashboard-admin-create-panel-discussion-form"
+          on:submit|preventDefault={save}>
+        <SpeakerTalkForm data={talkData}
+                         tags={data.tags}
+                         talkDurations={data.durations}
+                         useForm={false} />
+        <PersonArray labelText="Mögliche Hosts:"
+                     bind:selected={selectedHosts}
+                     data={data.possibleHosts} />
+        <Button classes="dashboard-admin-create-panel-discussion-button"
+                type="submit"
+                ariaLabel="Klicke hier, um den die neue Gesprächsrunde zu speichern">Speichern
+        </Button>
+    </form>
 </SectionDashboard>
 
 
 <style>
+    .dashboard-admin-create-panel-discussion-form {
+        display:        flex;
+        flex-direction: column;
+        gap:            var(--full-gap);
+    }
 
+    :global(.dashboard-admin-create-panel-discussion-button) {
+        align-self: center;
+        margin-top: var(--2x-margin);
+    }
 </style>
