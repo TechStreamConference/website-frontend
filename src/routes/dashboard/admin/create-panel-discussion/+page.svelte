b8e87d75725d8f6897244fdb53a4579f97e9ff3a
Martin <martin.schreurs@netcologne.de>
2025-02-17 20:48:19 +0100

    feat: navigation

--- src/routes/dashboard/admin/create-panel-discussion/+page.svelte
+++ src/routes/dashboard/admin/create-panel-discussion/+page.svelte
@@ -6,8 +6,23 @@
 
     import Tabs from 'elements/navigation/tabs.svelte';
     import SectionDashboard from 'elements/section/sectionDashboard.svelte';
+    import NavigationDropDown from 'elements/navigation/navigationDropDown.svelte';
+    import { getDropDownEntriesWithID, getIdFromDropDownEntry } from 'helper/navigation';
+    import { error } from '@sveltejs/kit';
+    import { loadPossibleHosts } from './panelHelper';
 
     export let data: LoadAdminCreatePanelDiscussion;
+
+    let currentEventId: number = 0;
+
+    async function onNavigate(dropDownEntry: string): Promise<void> {
+        currentEventId = getIdFromDropDownEntry(dropDownEntry);
+        if (currentEventId === 0) {
+            throw error(404);
+        }
+
+        data.possibleHosts = await loadPossibleHosts(fetch, currentEventId);
+    }
 </script>
 
 <Tabs classes="navigation-tabs-dashboard-subpage"
@@ -15,7 +30,10 @@
       entryName={MenuItem.adminCreatePanelDiscussion.name} />
 
 <SectionDashboard classes="standard-dashboard-section">
-
+    <NavigationDropDown labelText="Aktuelles Jahr:"
+                        id="dashboard-admin-create-panel-discussion-event-drop-down"
+                        data={getDropDownEntriesWithID(data.allEvents)}
+                        on:navigated={(e) => {onNavigate(e.detail)}} />
 </SectionDashboard>
 
 
