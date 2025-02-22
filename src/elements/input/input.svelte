e278fc689bcd489f1a92e966d93d6ed130a72b2b
Martin <martin.schreurs@netcologne.de>
2025-02-22 15:43:42 +0100

    feat: limit in text line

--- src/elements/input/input.svelte
+++ src/elements/input/input.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+    import type { Color } from 'helper/basic';
+
     import { typeWorkaround } from 'types/workaround';
+    import { lerpColor, orange } from 'helper/basic';
+    import { onMount } from 'svelte';
 
     import Label from 'elements/text/label.svelte';
+    import TextLine from 'elements/text/textLine.svelte';
 
     export let ariaLabel: string;
 
@@ -11,58 +16,106 @@
     export let labelText: string       = '';
     export let placeholderText: string = '';
     export let fileAccept: string      = '';
+    export let limit: number           = -1;
 
     export let value: string | number | File = '';
+    let colorString: string                  = '';
 
     let input: HTMLInputElement;
+
+    onMount(() => {
+        calcColor();
+    });
 
     export function clear(): void {
         input.value = '';
     }
+
+    function calcColor(): void {
+        if (limit === -1) {
+            return;
+        }
+        if (typeof value !== 'string' && typeof value !== 'number') {
+            return;
+        }
+
+        const length = typeof value === 'string' ? value.length : value.toString().length;
+        let factor   = 1 - (length / limit);
+        if (factor > 0.5) {
+            factor = 1;
+        } else {
+            factor *= 2;
+        }
+
+        const color: Color = lerpColor(orange, {
+            red:   255,
+            green: 255,
+            blue:  255,
+        }, factor);
+        colorString        = `rgb(${color.red},${color.green}, ${color.blue})`;
+    }
 </script>
 
-<div class={classes}>
+<div class="{classes} input-line-wrapper">
     <Label for_={id}>{labelText}</Label>
-    {#if type === 'file'}
-        <input
-              bind:this={input}
-              class="normal-font"
-              {id}
-              type="file"
-              accept={fileAccept}
-              name={id}
-              placeholder={placeholderText}
-              bind:value
-              on:input
-              on:change
-              aria-label={ariaLabel}
-              step="1"
-        />
-    {:else}
-        <input
-              bind:this={input}
-              class="normal-font"
-              {id}
-              use:typeWorkaround={type}
-              name={id}
-              placeholder={placeholderText}
-              bind:value
-              on:input
-              on:change
-              aria-label={ariaLabel}
-              step="1"
-        />
-    {/if}
+    <div class="input-line-entry-wrapper">
+        {#if type === 'file'}
+            <input
+                  bind:this={input}
+                  class="normal-font input-line"
+                  {id}
+                  type="file"
+                  accept={fileAccept}
+                  name={id}
+                  placeholder={placeholderText}
+                  bind:value
+                  on:input
+                  on:change
+                  aria-label={ariaLabel}
+                  step="1"
+            />
+        {:else}
+            <input
+                  bind:this={input}
+                  class="normal-font input-line"
+                  {id}
+                  use:typeWorkaround={type}
+                  maxlength={limit}
+                  name={id}
+                  placeholder={placeholderText}
+                  bind:value
+                  on:input={calcColor}
+                  on:change
+                  aria-label={ariaLabel}
+                  step="1"
+            />
+        {/if}
+        {#if limit !== -1}
+            {#if typeof value === 'string'}
+                <TextLine classes="input-limit-text text-color-custom"
+                          --color={colorString}>{value.length} / {limit}</TextLine>
+            {:else if typeof value === 'number'}
+                <TextLine classes="input-limit-text text-color-custom"
+                          --color={colorString}>{value.toString().length} / {limit}</TextLine>
+            {/if}
+        {/if}
+    </div>
 </div>
 
 <style>
-    div {
+    .input-line-wrapper {
         display:        flex;
         flex-direction: column;
         width:          100%;
     }
 
-    input {
+    .input-line-entry-wrapper {
+        display:        flex;
+        flex-direction: row;
+        position:       relative;
+    }
+
+    .input-line {
         color:            var(--white-color);
         background-color: var(--primary-color-light);
         border-radius:    var(--border-radius);
@@ -70,5 +123,12 @@
         padding:          var(--half-padding);
         width:            100%;
         font-size:        var(--full-font-size);
+    }
+
+    :global(.input-limit-text) {
+        position:  absolute;
+        top:       50%;
+        right:     1rem;
+        translate: 0 -50%;
     }
 </style>
