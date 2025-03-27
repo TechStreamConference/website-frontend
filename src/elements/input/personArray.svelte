0b504d30c6dec4a487552e780e41b26b5e23afed
Martin <martin.schreurs@netcologne.de>
2025-03-27 22:46:53 +0100

    fix: invisible tags in person array

--- src/elements/input/personArray.svelte
+++ src/elements/input/personArray.svelte
@@ -70,7 +70,7 @@
                 <Icon color="red"
                       type="CircleCross" />
                 <Tag text={person.name}
-                     --tag-text-color="#FFF"
+                     --tag-text-color="var(--text-color)"
                      --tag-background-color="#FFFFFF00"
                 />
             </div>
