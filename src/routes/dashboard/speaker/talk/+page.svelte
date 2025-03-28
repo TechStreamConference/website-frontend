13c0580e1bb390c55420f442f871cb7c88c1228e
PurpurTentakel97 <martin.schreurs@netcologne.de>
2025-03-28 21:09:26 +0100

    fix: tabs width override

--- src/routes/dashboard/speaker/talk/+page.svelte
+++ src/routes/dashboard/speaker/talk/+page.svelte
@@ -77,7 +77,7 @@
 </script>
 
 
-<Tabs classes="subpage-navigation-tabs"
+<Tabs classes="subpage-navigation-tabs subpage-navigation-tabs-wide-tabs-override"
       position="center"
       entries={Menu.speaker}
       entryName={MenuItem.speakerTalk.name} />
