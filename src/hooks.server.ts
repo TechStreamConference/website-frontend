1890b889904b7afa02599e8405a314701224b330
Michael Gerhold <michael.gerhold@web.de>
2025-03-30 12:47:55 +0200

    feat: always include `X-Real-Ip` header in SSR fetch requests

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,10 @@
-import type { HandleFetch } from '@sveltejs/kit';
+import {error, type HandleFetch} from '@sveltejs/kit';
 import { env } from '$env/dynamic/public';
+import {dev} from "$app/environment";
 
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
     const cookies = event.request.headers.get("cookie")
+    console.log(`[handleFetch] cookies: ${cookies}`);
     if (!env.PUBLIC_API_BASE_URL_SSR) {
         console.error('[handleFetch] env.PUBLIC_API_BASE_URL_SSR is not set.');
         return fetch(request);
@@ -18,6 +20,16 @@ export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
         request
     );
     request.headers.set('cookie', cookies!);
-    request.headers.set('x-real-ip', event.request.headers.get('x-real-ip') || '');
+
+    if (dev) {
+        request.headers.set('x-real-ip', '127.0.0.1');
+    } else {
+        const ip = event.request.headers.get('x-real-ip');
+        if (!ip) {
+            console.error('[handleFetch] x-real-ip header is missing.');
+            throw error(500);
+        }
+        request.headers.set('x-real-ip', ip);
+    }
     return fetch(request);
 };
