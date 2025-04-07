fe0a9f23fd95bab2b3c9b01ee61aee4969a31c6b
PurpurTentakel97 <martin.schreurs@netcologne.de>
2025-04-07 20:06:52 +0200

    fix: remove smaller text for mobile

--- src/elements/text/subHeadline.svelte
+++ src/elements/text/subHeadline.svelte
@@ -27,16 +27,4 @@
     .sub-headline-white {
         color: var(--white-color);
     }
-
-    @media (max-width: 600px) {
-        .sub-headline-big {
-            font-size:   var(--full-font-size);
-            line-height: var(--full-line-height);
-        }
-
-        .sub-headline-small {
-            font-size:   var(--half-font-size);
-            line-height: var(--half-line-height);
-        }
-    }
 </style>
