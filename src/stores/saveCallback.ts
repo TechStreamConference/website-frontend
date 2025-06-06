417c577acd4aa85c1d785a9cb1a32d2d3c2e877e
PurpurTentakel97 <martin.schreurs@netcologne.de>
2025-06-06 11:01:43 +0200

    feat: add existing stores

---
+++ src/stores/saveCallback.ts
@@ -0,0 +1,18 @@
+import { writable, get, type Writable } from 'svelte/store';
+
+type ValueType =
+    (() => Promise<boolean>)
+    | undefined;
+const _callback: Writable<ValueType> = writable(undefined);
+
+export function saveCallback(): ValueType {
+    return get(_callback);
+}
+
+export function resetSaveCallback(): void {
+    _callback.set(undefined);
+}
+
+export function setSaveCallback(callback: () => Promise<boolean>): void {
+    _callback.set(callback);
+}
