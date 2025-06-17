65d79d7ea07da2276fc2fc4182476c1e69fa834b
PurpurTentakel97 <martin.schreurs@netcologne.de>
2025-06-17 12:44:31 +0200

    feat: implement dynamic tag colors based on light/dark theme detection

--- src/lookup/tagColorLookup.ts
+++ src/lookup/tagColorLookup.ts
@@ -1,7 +1,23 @@
+import {isDark} from "stores/theme";
+
 // @formatter:off
-const colors: string[] = [
+const colorsDark: string[] = [
     "#00ff00", "#006400", // 1 green
     "#00ffff", "#00c8c8", // 3 cyan !important
+    "#0000ff", "#000096", // 5 blue
+    "#00ff00", "#006400", // 7 green
+    "#000000", "#323232", // 9 black
+    "#0000ff", "#000096", // 11 blue
+    "#ffff38", "#969600", // 13 yellow
+    "#000000", "#323232", // 15 black !important
+    "#00ff00", "#006400", // 17 green !important
+    "#c80000", "#960000", // 19 red
+    "#c800c8", "#960096", // 21 magenta
+    "#c80000", "#960000", // 23 red !important
+];
+const colorsLight: string[] = [
+    "#00ff00", "#006400", // 1 green
+    "#d50808", "#e80000", // 3 cyan !important
     "#0000ff", "#000096", // 5 blue
     "#00ff00", "#006400", // 7 green
     "#000000", "#323232", // 9 black
@@ -25,9 +41,9 @@ function hexToRgb(hex: string): {
 
     // Parse r, g, b
     const bigint = parseInt(cleanHex, 16);
-    const r      = (bigint >> 16) & 255;
-    const g      = (bigint >> 8) & 255;
-    const b      = bigint & 255;
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
 
     return {
         r,
@@ -36,21 +52,25 @@ function hexToRgb(hex: string): {
     };
 }
 
+function currentLookUp(): string[] {
+    return isDark() ? colorsDark : colorsLight;
+}
+
 function keyFromIndex(index: number): number {
-    return ((index - 1) % Object.keys(colors).length);
+    return ((index - 1) % Object.keys(currentLookUp()).length);
 }
 
 export function tagColorLookup(key: number): string {
-    return colors[keyFromIndex(key)];
+    return currentLookUp()[keyFromIndex(key)];
 }
 
 export function tagTextColorLookup(key: number): string {
-    const color     = colors[keyFromIndex(key)];
+    const color = currentLookUp()[keyFromIndex(key)];
     const {
-              r,
-              g,
-              b,
-          }         = hexToRgb(color);
+        r,
+        g,
+        b,
+    } = hexToRgb(color);
     const luminance = 0.299 * r + 0.587 * g + 0.114 * b;
     return luminance > 128 ? '#000' : '#fff';
 }
