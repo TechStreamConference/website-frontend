51b2e8a3e29084049bd7e38973eb0b0234ed8d29
PurpurTentakel97 <martin.schreurs@netcologne.de>
2025-07-19 12:56:41 +0200

    chore: remove debug `console.log` statements from `preventAutoCollapsePopupStore`

--- src/stores/preventAutoCollapsePopupStore.ts
+++ src/stores/preventAutoCollapsePopupStore.ts
@@ -4,21 +4,17 @@ type ValueType = boolean;
 const _preventAutoCollapsePopup: Writable<ValueType> = writable(false);
 
 export function preventAutoCollapsePopup(): boolean {
-    console.log('preventAutoCollapsePopup');
     return get(_preventAutoCollapsePopup);
 }
 
 export function setPreventAutoCollapsePopup(): void {
     _preventAutoCollapsePopup.set(true);
-    console.log('setPreventAutoCollapsePopup');
 }
 
 export function resetPreventAutoCollapsePopup(): void {
     _preventAutoCollapsePopup.set(false);
-    console.log('resetPreventAutoCollapsePopup');
 }
 
 export function resetPreventAutoCollapsePopupWithDelay(delay: number): void {
     setTimeout(resetPreventAutoCollapsePopup, delay);
-    console.log('resetPreventAutoCollapsePopupWithDelay');
 }
