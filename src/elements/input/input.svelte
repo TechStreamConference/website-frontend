827cb6fc84c51e9f034be39114f17b4d7dad4fc1
PurpurTentakel97 <martin.schreurs@netcologne.de>
2025-07-23 23:41:56 +0200

    feat: switch to new input

--- src/elements/input/input.svelte
+++ src/elements/input/input.svelte
@@ -1,32 +1,36 @@
 <script lang="ts">
-    import {typeWorkaround} from 'types/workaround';
-    import {lerpTextCountColor} from 'helper/color';
-    import {onMount} from 'svelte';
-    import {createEventDispatcher} from 'svelte';
+    import Label from "elements/text/label.svelte"
+    import TextLine from "elements/text/textLine.svelte"
 
-    import Label from 'elements/text/label.svelte';
-    import TextLine from 'elements/text/textLine.svelte';
+    import {createEventDispatcher, onMount} from "svelte";
+    import {typeWorkaround} from "types/workaround";
+    import {lerpTextCountColor} from "helper/color";
 
-    export let ariaLabel: string;
+    export let labelText = "";
 
-    export let classes: string = '';
-    export let type: string = '';
-    export let id: string = '';
-    export let labelText: string = '';
-    export let placeholderText: string = '';
-    export let fileAccept: string = '';
+    export let id;
+    export let value: string | number | File = "";
+    export let ariaLabel = "";
+    export let classes = "";
+    export let type = "string";
     export const NO_LIMIT: number = -1;
     export let limit: number = NO_LIMIT;
+    export let placeholderText: string = " ";
 
-    export let value: string | number | File = '';
-    let colorString: string = '';
+    export let fileAccept: string = "";
 
+    $: if (placeholderText.trim().length == 0) {
+        placeholderText = " ";
+    }
+
+    let colorString: string;
     let input: HTMLInputElement;
     const dispatch = createEventDispatcher();
 
     onMount(() => {
         calcColor();
-    });
+    })
+
 
     export function clear(): void {
         input.value = '';
@@ -44,31 +48,20 @@
         const color = lerpTextCountColor(length, limit);
         colorString = `rgb(${color.red},${color.green}, ${color.blue})`;
     }
+
 </script>
 
 <div class="{classes} input-line-wrapper">
-    <div class="input-line-label-wrapper">
-        <Label for_={id}>{labelText}</Label>
-        {#if limit !== NO_LIMIT}
-            {#if typeof value === 'string'}
-                <TextLine classes="text-color-custom"
-                          --color={colorString}>{value.length} / {limit}</TextLine>
-            {:else if typeof value === 'number'}
-                <TextLine classes="text-color-custom"
-                          --color={colorString}>{value.toString().length} / {limit}</TextLine>
-            {/if}
-        {/if}
-    </div>
-    {#if type === 'file'}
+    {#if type === "file"}
         <input
                 bind:this={input}
-                class="normal-font input-line"
+                class="normal-font"
                 {id}
                 type="file"
                 accept={fileAccept}
                 name={id}
-                placeholder={placeholderText}
                 bind:value
+                placeholder={placeholderText}
                 on:input
                 on:change
                 aria-label={ariaLabel}
@@ -77,41 +70,91 @@
     {:else}
         <input
                 bind:this={input}
-                class="normal-font input-line"
+                class="normal-font"
                 {id}
                 use:typeWorkaround={type}
                 maxlength={limit}
-                name={id}
-                placeholder={placeholderText}
                 bind:value
-                on:input={ () => { calcColor(); dispatch('input'); } }
+                placeholder={placeholderText}
+                on:input={() => {calcColor(); dispatch("input");}}
                 on:change
                 aria-label={ariaLabel}
                 step="1"
         />
     {/if}
+    <Label for_="{id}" classes="input-floating-label">{labelText}</Label>
+    {#if limit !== NO_LIMIT}
+        {#if typeof value === "string"}
+            <TextLine
+                    classes="text-color-custom input-limit-text-line"
+                    --color="{colorString}"
+            >{value.length} / {limit}</TextLine>
+        {:else if typeof value === "number"}
+            <TextLine
+                    classes="text-color-custom input-limit-text-line"
+                    --color="{colorString}"
+            >{value.toString().length} / {limit}</TextLine>
+        {/if}
+
+    {/if}
 </div>
 
 <style>
     .input-line-wrapper {
-        display: flex;
-        flex-direction: column;
         width: 100%;
+        height: fit-content;
+        position: relative;
+        margin-top: var(--full-margin);
     }
 
-    .input-line-label-wrapper {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-    }
-
-    .input-line {
-        color: var(--white-color);
-        background-color: var(--primary-color-light);
-        border-radius: var(--border-radius);
-        border: none;
+    input {
+        background-color: transparent;
         padding: var(--half-padding);
         width: 100%;
         font-size: var(--full-font-size);
+        border-radius: var(--border-radius);
+        border: 1px solid var(--line-color);
+        transition: border-radius var(--transition-duration);
+    }
+
+    input:hover {
+        border-radius: 0;
+        transition: border-radius var(--transition-duration);
+    }
+
+
+    :global(.input-limit-text-line) {
+        position: absolute;
+        top: -1.5rem;
+        right: 1rem;
+        font-size: var(--half-font-size) !important;
+        background-color: var(--background-color);
+        padding: 0 var(--full-padding);
+        transition: top var(--transition-duration),
+        left var(--transition-duration),
+        font-size var(--transition-duration);
+        pointer-events: none;
+    }
+
+    :global(.input-floating-label) {
+        position: absolute;
+        left: var(--half-padding);
+        top: 50%;
+        transform: translateY(-50%);
+        font-size: var(--full-font-size);
+        background-color: var(--background-color);
+        padding: 0 var(--half-padding);
+        border-radius: var(--border-radius);
+        transition: top var(--transition-duration),
+        left var(--transition-duration),
+        font-size var(--transition-duration);
+    }
+
+    input:focus ~ :global(.input-floating-label),
+    input:not(:placeholder-shown) ~ :global(.input-floating-label),
+    input:hover ~ :global(.input-floating-label) {
+        top: -0.7rem;
+        left: 0.5rem;
+        font-size: var(--half-font-size);
     }
 </style>
